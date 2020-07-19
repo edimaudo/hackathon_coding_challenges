bf9420725edc0ca46f08913b618401dd24aa3f1c
Edima <edimaudo@gmail.com>
2020-07-19 07:05:48 -0400

    Update akmeed.R

--- Zindi/akmeed_challenge/akmeed.R
+++ Zindi/akmeed_challenge/akmeed.R
@@ -10,3 +10,13 @@ for (package in packages) {
   }
 }
 
+#load data
+
+orders <- read.csv("orders.csv")
+vendors <- read.csv("vendors.csv")
+test_customers <- read.csv("test_customers.csv")
+test_locations <- read.csv("test_locations.csv")
+train_customers <- read.csv("train_customers.csv")
+train_locations <- read.csv("train_locations.csv")
+sample_submission <- read.csv("SampleSubmission.csv")
+
