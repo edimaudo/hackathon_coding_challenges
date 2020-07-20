0f5f84ca11a594c6f0ea5ac3ddf0ec8a5c9a1346
Edima <edimaudo@gmail.com>
2020-07-20 18:37:45 -0400

    Update akeed.R

--- Zindi/akeed_challenge/akeed.R
+++ Zindi/akeed_challenge/akeed.R
@@ -11,7 +11,6 @@ for (package in packages) {
 }
 
 #load data
-
 orders <- read.csv("orders.csv")
 vendors <- read.csv("vendors.csv")
 test_customers <- read.csv("test_customers.csv")
@@ -20,3 +19,20 @@ train_customers <- read.csv("train_customers.csv")
 train_locations <- read.csv("train_locations.csv")
 sample_submission <- read.csv("SampleSubmission.csv")
 
+#data summary
+glimpse(orders)
+summary(orders)
+
+glimpse(vendors)
+summary(vendors)
+
+#rename columns
+colnames(train_locations) <- c("customer_id","location_number",
+                               "location_type","latitude","longitude")
+
+colnames(train_customers) <- c("customer_id","gender","dob","status",
+                               "verified","language","created_at","updated_at")
+
+#combine data
+train_info <- train_customers %>%
+  inner_join(train_locations,)
