fcc9d00e2c6b5307cd352413827ff9f245ecd375
Edima <edimaudo@gmail.com>
2020-07-22 22:19:24 -0400

    Update cdp.R

--- cdp_hackathon/cdp.R
+++ cdp_hackathon/cdp.R
@@ -16,14 +16,36 @@ for (package in packages) {
 #load data
 Cities_Data_mb2_2017_2019 <- read.csv("Cities_Data_2017-2019_mb2.csv")
 CorporateCC_mb2_2018_2019 <- read.csv("2018-2019_CorporateCC_mb2.csv")
-CorporateCC_mb2_2015_2017 <- read.csv("2015-2017_CorporateCC_mb2.csv")
 
 #data summary
 summary(Cities_Data_mb2_2017_2019)
 summary(CorporateCC_mb2_2018_2019)
 
+#remove 2017 data for cities
+Cities_Data_mb2_2018_2019 <- Cities_Data_mb2_2017_2019 %>%
+  filter(Project.Year %in% c('2018','2019'))
 
+#city data cleanup and tidying
 
-#load external data
+#city sentimental analysis
 
+#city topic modeling
+
+#coporation data cleanup and tidying
+
+#coporation sentiment aanlysis
+
+#coporation topic modeling
+
+#merge coporation and city information - #load external data - kaggle site
+#https://www.kaggle.com/peopledatalabssf/free-7-million-company-dataset
+
+#get KPIs
+#https://datacatalog.worldbank.org/dataset/curb-climate-action-urban-sustainability
+#https://datacatalog.worldbank.org/dataset/environment-social-and-governance-data
+
+#build KPI model
+
+#dashboard in shiny showing city information, coporation information
+#kpi information
 
