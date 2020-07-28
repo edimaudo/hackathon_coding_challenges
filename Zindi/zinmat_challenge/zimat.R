dbc377ff874d68c3e7d220999a37e90ad7666ecc
Edima <edimaudo@gmail.com>
2020-07-27 21:21:03 -0400

    Update zimat.R

--- Zindi/zinmat_challenge/zimat.R
+++ Zindi/zinmat_challenge/zimat.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 
 #packages 
-packages <- c('ggplot2', 'corrplot','tidyverse','ggfortify','readxl','DT',
+packages <- c('ggplot2', 'corrplot','tidyverse','ggfortify','readxl','dplyr',
               'caret','mlbench','mice','scales','recommenderlab','proxy','reshape2',
               'caTools','dummies','highcharter',"gridExtra")
 #load packages
@@ -17,3 +17,7 @@ for (package in packages) {
 train <- read.csv("Train.csv")
 test <- read.csv("Test.csv")
 sample_submission <- read.csv("SampleSubmission.csv")
+
+products <- c('P5DA', 'RIBP', '8NN1',
+              '7POT', '66FJ', 'GYSR', 'SOP4', 'RVSZ', 'PYUQ', 'LJR9', 'N2MW', 'AHXO',
+              'BSTQ', 'FM3X', 'K6QO', 'QBOL', 'JWFN', 'JZ9D', 'J9JW', 'GHYX', 'ECY3')
