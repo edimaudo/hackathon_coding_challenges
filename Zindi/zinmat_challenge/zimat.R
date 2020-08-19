3fa3353616a38b959778115a80d106849c1bc28a
Edima <edimaudo@gmail.com>
2020-08-19 12:55:51 -0400

    Update zimat.R

--- Zindi/zinmat_challenge/zimat.R
+++ Zindi/zinmat_challenge/zimat.R
@@ -4,7 +4,7 @@ rm(list = ls())
 #packages 
 packages <- c('ggplot2', 'corrplot','tidyverse','ggfortify','readxl','dplyr',
               'caret','mlbench','mice','scales','recommenderlab','proxy','reshape2',
-              'caTools','dummies','highcharter',"gridExtra")
+              'caTools','dummies','highcharter',"gridExtra",'scales','catboost')
 #load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
