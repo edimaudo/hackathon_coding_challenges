e4c48f211598be7aaac58c8c5bc37985c57c8c45
Edima <edimaudo@gmail.com>
2020-09-02 20:05:00 -0400

    Update zimat.R

--- Zindi/zinmat_challenge/zimat.R
+++ Zindi/zinmat_challenge/zimat.R
@@ -60,31 +60,40 @@ ratingmat_test = as(ratingmat_test, "binaryRatingMatrix")
 # Modeling
 # =======================================================
 
-
-# IBCF modeling
- models_to_evaluate = list(IBCF_cos = list(name = "IBCF", param = list(method = "cosine")),
-                           IBCF_pea = list(name = "IBCF", param = list(method = "pearson")))
+# IBCF modeling#
+# models_to_evaluate = list(IBCF_cos = list(name = "IBCF", param = list(method = "cosine")),
+#                           IBCF_pea = list(name = "IBCF", param = list(method = "pearson")))
 #IBCF cos better
 
 # UBCF modeling
-models_to_evaluate = list(UBCF_cos = list(name = "UBCF", param = list(method = "cosine")),
-                          UBCF_pea = list(name = "UBCF", param = list(method = "pearson")))
-#UBCF pearson
+#models_to_evaluate = list(UBCF_cos = list(name = "UBCF", param = list(method = "cosine")),
+#                          UBCF_pea = list(name = "UBCF", param = list(method = "pearson")))
+#UBCF pearson better
 
 # Random & popular modeling
-models_to_evaluate = list(RANDOM = list(name = "RANDOM", param = NULL),
-                          POPULAR = list(name = "POPULAR", param = NULL))
+#models_to_evaluate = list(RANDOM = list(name = "RANDOM", param = NULL),
+#                          POPULAR = list(name = "POPULAR", param = NULL))
 #popular was better
 
 #IBCF and UBCF
-models_to_evaluate = list(IBCF = list(name = "IBCF", param = NULL),
-                          UBCF = list(name = "UBCF", param = NULL))
+#models_to_evaluate = list(IBCF = list(name = "IBCF", param = NULL),
+#                          UBCF = list(name = "UBCF", param = NULL))
 #IBCF better
 
 #IBCF cos and UBCF pearson
-models_to_evaluate = list(IBCF_cos = list(name = "IBCF", param = list(method = "cosine")),
-                          UBCF_pea = list(name = "UBCF", param = list(method = "pearson")))
+#models_to_evaluate = list(IBCF_cos = list(name = "IBCF", param = list(method = "cosine")),
+#                          UBCF_pea = list(name = "UBCF", param = list(method = "pearson")))
+
 #UBCF pearson was better
+
+# models_to_evaluate = list(IBCF = list(name = "IBCF", param = NULL),
+#                           POPULAR = list(name = "POPULAR", param = NULL))
+#IBCF better
+
+#IBCF and UBCF pearson
+models_to_evaluate = list(IBCF_cos = list(name = "IBCF", param = NULL),
+                          UBCF_pea = list(name = "UBCF", param = list(method = "pearson")))
+#ibcf cos better
 
 #recommender type outputs
 n_recommendations = c(1,2,3,4,5)
@@ -103,47 +112,55 @@ title("Precision-Recall")
 ## =======================================================
 # Current recommendation model 
 # =======================================================
+items_to_recommend <- 1
+eval_recommender = Recommender(data = ratingmat_train,
+                               method = "IBCF", 
+                               param=list(method="Cosine",k=10, alpha = 0.9)) 
+eval_prediction = predict(object = eval_recommender,
+                          newdata = ratingmat_test,
+                          n = items_to_recommend)
 
-# # =======================================================
-# # output
-# # =======================================================
-# 
-# submission_info1 <- sample_submission
-# submission_info1$ID <- substr(submission_info1$ID.X.PCODE,1,7)
-# submission_info1$Product <- substr(submission_info1$ID.X.PCODE,11,14)
-# 
-# rec <-  as(eval_prediction, "list")
-# 
-# submission_info2 <- data.frame(matrix(ncol = 4, nrow = 0))
-# colnames(submission_info2) <- c("ID.X.PCODE","Label","ID","Product")
-#  
-#  for (i in 1:length(rec)){
-#      temp <- submission_info1 %>%
-#      filter(ID == test_df$ID[i]) %>%
-#      filter(Product %in% c(rec[i][[1]])) %>%
-#      mutate (Label = 1)
-#      temp <- data.frame(temp)
-#      submission_info2 <- rbind(submission_info2, temp)
-#  }
-# 
-# 
-#  
-# submission_info3 <- submission_info1 %>%
-#    left_join(submission_info2, by="ID.X.PCODE")
-#  
-# submission_info3[is.na(submission_info3)] <- 0
-# submission_info3$Label <- ifelse(submission_info3$Label.y==1, 1, 0)
-# submission_info3$Label <- as.integer(submission_info3$Label)
-# 
-# final_submission <- submission_info3 %>%
-#   select(ID.X.PCODE,Label)
-# write.csv(final_submission,"output_ibcf_cos4.csv",row.names = F)
-# 
-
-
+rec <-  as(eval_prediction, "list")
 # =======================================================
-# Selected recommendation model 
+# output
 # =======================================================
+submission_info1 <- sample_submission
+submission_info1$ID <- substr(submission_info1$ID.X.PCODE,1,7)
+submission_info1$Product <- substr(submission_info1$ID.X.PCODE,11,14)
+
+submission_info2 <- data.frame(matrix(ncol = 4, nrow = 0))
+colnames(submission_info2) <- c("ID.X.PCODE","Label","ID","Product")
+
+for (i in 1:length(rec)){
+  temp <- submission_info1 %>%
+    filter(ID == test_df$ID[i]) %>%
+    filter(Product %in% c(rec[i][[1]])) %>%
+    mutate (Label = 1)
+  temp <- data.frame(temp)
+  submission_info2 <- rbind(submission_info2, temp)
+}
+
+
+
+submission_info3 <- submission_info1 %>%
+  left_join(submission_info2, by="ID.X.PCODE")
+
+submission_info3[is.na(submission_info3)] <- 0
+submission_info3$Label <- ifelse(submission_info3$Label.y==1, 1, 0)
+submission_info3$Label <- as.integer(submission_info3$Label)
+
+final_submission <- submission_info3 %>%
+  select(ID.X.PCODE,Label)
+write.csv(final_submission,"output_ibcf_cos_k_10_alpha_9.csv",row.names = F)
+
+
+
+
+
+
+
+
+
 
  
  
