912d14d62090e3125b5b6eb3efc6f0599c594c34
Edima <edimaudo@gmail.com>
2021-02-27 14:08:49 -0500

    Create A12S.R

---
+++ A12S/A12S.R
@@ -0,0 +1,20 @@
+#===================
+## Load Libraries
+#===================
+rm(list = ls()) #clear environment
+
+# libraries
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
+              ,'FactoMineR','factoextra','readxl','scales','dplyr','mlbench','caTools',
+              'gridExtra','doParallel')
+# load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#===================
+## Load data
+#===================
