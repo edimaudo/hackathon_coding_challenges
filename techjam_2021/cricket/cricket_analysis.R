659b02c6ddd03fa3304e097b4372da0f8d517c76
Edima <edimaudo@gmail.com>
2021-07-19 12:39:00 -0400

    Update cricket_analysis.R

--- techjam_2021/cricket/cricket_analysis.R
+++ techjam_2021/cricket/cricket_analysis.R
@@ -3,10 +3,9 @@ rm(list = ls()) #clear environment
 #===================
 # Packages
 #===================
-packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies",'readxl',
-              'scales','dplyr','mlbench','caTools','forecast','TTR','xts',
-              'FactoMineR','factoextra',"fastDummies",'scales','dplyr','mlbench',
-              'caTools','gridExtra','doParallel','lubridate','data.table')
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies",'mlbench',
+              'caTools','doParallel','scales','dplyr','readxl','FactoMineR','factoextra',
+              'gridExtra','lubridate','data.table','forecast','TTR','xts')
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
     install.packages(package)
@@ -17,26 +16,48 @@ for (package in packages) {
 #===================
 # Load Data
 #===================
-ball_delivery_df <- read.delim("BallByBall_DeliveryDetails_BBL&WBBL.txt")
-ball_fixtures_df  <- read.delim("BallByBall_FixtureDetails_BBL&WBBL.txt")
-ball_players_df <- read.delim("BallByBall_PlayerDetails_BBL&WBBL.txt")
-ball_venue_df <- read.delim("BallByBall_VenueDetails_BBL&WBBL.txt")
+# ball_delivery_df <- read.delim("BallByBall_DeliveryDetails_BBL&WBBL.txt")
+# ball_fixtures_df  <- read.delim("BallByBall_FixtureDetails_BBL&WBBL.txt")
+# ball_players_df <- read.delim("BallByBall_PlayerDetails_BBL&WBBL.txt")
+# ball_venue_df <- read.delim("BallByBall_VenueDetails_BBL&WBBL.txt")
+# 
+# matches_df <- read.delim("Matches.txt")
+# match_players_df <- read.delim("MatchPlayers.txt")
+# 
+# ovals_facility_df <- read.delim("OvalsAndFacility.txt")
+# ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
+# 
+# players_df <- read.delim("Players.txt")
+# 
+# #large data
+# deliveries_df <- fread("Deliveries.txt")
+# ticket_sales_df <- fread("TicketSales_2016-17SeasonOnwards.txt")
 
-matches_df <- read.delim("Matches.txt")
-match_players_df <- read.delim("MatchPlayers.txt")
 
+
+#===================
+# grounds
+#===================
 ovals_facility_df <- read.delim("OvalsAndFacility.txt")
 ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
 
-players_df <- read.delim("Players.txt")
+#fix column names
 
-#large data
-deliveries_df <- fread("Deliveries.txt")
-ticket_sales_df <- fread("TicketSales_2016-17SeasonOnwards.txt")
+# Grounds 
+# - Insights into the grounds
+# - ground revenue
+# - possible whether 
+# - **DRS**: Improve on field performance by leveraging umpire,
+# grounds and match data to make decision-making easier for 
+# cricket players and leaders. The idea behind this solution is to highlight areas where 
+# umpires, and possibly even the team captains, are making the wrong decisions 
+
+ovals_facility_df
+types of grounds and other parts of the data
+performance on the ground - combine with match(
+performance based on dollars - revenue
+)
 
 
-
-#===================
-# Check for missing information
-#===================
-
+# Revenue
+#so that we can better prepare them for future matches.
