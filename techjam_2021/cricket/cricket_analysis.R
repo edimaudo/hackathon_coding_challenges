44e4653523203869b65df62892489706d3953699
Edima <edimaudo@gmail.com>
2021-07-20 19:23:48 -0400

    Update cricket_analysis.R

--- techjam_2021/cricket/cricket_analysis.R
+++ techjam_2021/cricket/cricket_analysis.R
@@ -14,15 +14,22 @@ for (package in packages) {
 }
 
 #===================
+# Player analytics
+#===================
+ball_players_df <- read.delim("BallByBall_PlayerDetails_BBL&WBBL.txt")
+match_players_df <- read.delim("MatchPlayers.txt")
+
+
+#===================
 # Load Data
 #===================
 # ball_delivery_df <- read.delim("BallByBall_DeliveryDetails_BBL&WBBL.txt")
 # ball_fixtures_df  <- read.delim("BallByBall_FixtureDetails_BBL&WBBL.txt")
-# ball_players_df <- read.delim("BallByBall_PlayerDetails_BBL&WBBL.txt")
+
 # ball_venue_df <- read.delim("BallByBall_VenueDetails_BBL&WBBL.txt")
 # 
 # matches_df <- read.delim("Matches.txt")
-# match_players_df <- read.delim("MatchPlayers.txt")
+
 # 
 # ovals_facility_df <- read.delim("OvalsAndFacility.txt")
 # ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
@@ -31,39 +38,30 @@ for (package in packages) {
 # 
 # #large data
 # deliveries_df <- fread("Deliveries.txt")
-# ticket_sales_df <- fread("TicketSales_2016-17SeasonOnwards.txt")
 
 
 
-#===================
-# grounds
-#===================
-matches_df <- read.delim("Matches.txt")
-ovals_facility_df <- read.delim("OvalsAndFacility.txt")
-ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
 
-#fix column names
-colnames(ovals_facility_df) <- c('PropertyID','PropertyName','PropertySuburb',
-                                 'PropertyRegion','FederalElectorateDescription',
-                                 'StateElectorateDescription','NumberOfOvals',
-                                 'PropertyState','PropertyPostcode',
-                                 'PropertyLatitude','PropertyLongitude',
-                                 'OvalID','MyCricketOvalID','OvalName',
-                                 'OvalLatitude','OvalLongitude',
-                                 'OvalDimension','PitchType')
+#matches_df <- read.delim("Matches.txt")
+# ovals_facility_df <- read.delim("OvalsAndFacility.txt")
+# ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
+# 
+# #fix column names
+# colnames(ovals_facility_df) <- c('PropertyID','PropertyName','PropertySuburb',
+#                                  'PropertyRegion','FederalElectorateDescription',
+#                                  'StateElectorateDescription','NumberOfOvals',
+#                                  'PropertyState','PropertyPostcode',
+#                                  'PropertyLatitude','PropertyLongitude',
+#                                  'OvalID','MyCricketOvalID','OvalName',
+#                                  'OvalLatitude','OvalLongitude',
+#                                  'OvalDimension','PitchType')
+# 
+# colnames(ovals_facility_audit_df) <- c('AuditID ','PropertyID','FacilityGroup',
+#                                        'AuditSectionName','QuestionID','AuditQuestion',
+#                                        'QuestionOrder','FacilityNumber','AuditResponse',
+#                                        'MeetsNationalCompliance')
 
-colnames(ovals_facility_audit_df) <- c('AuditID ','PropertyID','FacilityGroup',
-                                       'AuditSectionName','QuestionID','AuditQuestion',
-                                       'QuestionOrder','FacilityNumber','AuditResponse',
-                                       'MeetsNationalCompliance')
 
-#**DRS**: Improve on field performance by leveraging umpire,
-# grounds and match data to make decision-making easier for 
-# cricket players and leaders. The idea behind this solution is to highlight areas where 
-# umpires, and possibly even the team captains, are making the wrong decisions 
-
-#What ar the performance metrics
-brainstorm - wins/loss ratio
 
 
 
