20ffc5e2daeb371d97ad5e73988964bea47e8621
Edima <edimaudo@gmail.com>
2021-07-21 17:26:32 -0400

    Update app.R

--- techjam_2021/cricket/app.R
+++ techjam_2021/cricket/app.R
@@ -37,12 +37,8 @@ ui <- dashboardPage(skin = "green",
     dashboardSidebar(
         sidebarMenu(
             menuItem("About", tabName = "about", icon = icon("far fa-address-card")),
-            #menuItem("Cricket Australia", tabName = "cricket_australia", icon = icon("th")),
-            #menuItem("Cricket Primer", tabName = "cricket_primer", icon = icon("th")),
-            menuItem("Grounds", tabName = "grounds", icon = icon("th")),
-            menuItem("Team", tabName = "team", icon = icon("th")),
             menuItem("Player", tabName = "player", icon = icon("th")),
-            menuItem("Match", tabName = "match", icon = icon("th"))
+            menuItem("Team", tabName = "team", icon = icon("th"))
         )
     ),
     dashboardBody(
@@ -52,11 +48,7 @@ ui <- dashboardPage(skin = "green",
                             #tableOutput("contents")
                         )
           ),
-        tabItem(tabName = "grounds",
-                mainPanel(
-                    #tableOutput("contents")
-                )
-        ),
+      
         tabItem(tabName = "team",
                 mainPanel(
                     #tableOutput("contents")
@@ -67,14 +59,8 @@ ui <- dashboardPage(skin = "green",
                     #tableOutput("contents")
                 )
         ),
-        tabItem(tabName = "match",
-                mainPanel(
-                    #tableOutput("contents")
-                )
+
         )
-        
-    )
-       
     )
 )  
 
