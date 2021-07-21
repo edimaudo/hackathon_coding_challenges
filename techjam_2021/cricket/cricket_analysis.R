1b8e094b421b3143c5fb918c932c32bbc199720c
Edima <edimaudo@gmail.com>
2021-07-20 20:03:26 -0400

    Update cricket_analysis.R

--- techjam_2021/cricket/cricket_analysis.R
+++ techjam_2021/cricket/cricket_analysis.R
@@ -13,54 +13,40 @@ for (package in packages) {
   }
 }
 
-#===================
-# Player analytics
-#===================
-ball_players_df <- read.delim("BallByBall_PlayerDetails_BBL&WBBL.txt")
-match_players_df <- read.delim("MatchPlayers.txt")
 
 
 #===================
 # Load Data
 #===================
-# ball_delivery_df <- read.delim("BallByBall_DeliveryDetails_BBL&WBBL.txt")
-# ball_fixtures_df  <- read.delim("BallByBall_FixtureDetails_BBL&WBBL.txt")
-
-# ball_venue_df <- read.delim("BallByBall_VenueDetails_BBL&WBBL.txt")
-# 
-# matches_df <- read.delim("Matches.txt")
-
-# 
-# ovals_facility_df <- read.delim("OvalsAndFacility.txt")
-# ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
-# 
-# players_df <- read.delim("Players.txt")
-# 
+players_df <- read.delim("Players.txt")
+ball_players_df <- read.delim("BallByBall_PlayerDetails_BBL&WBBL.txt")
+match_players_df <- read.delim("MatchPlayers.txt")
+matches_df <- read.delim("Matches.txt")
+ball_delivery_df <- read.delim("BallByBall_DeliveryDetails_BBL&WBBL.txt")
+ball_fixtures_df  <- read.delim("BallByBall_FixtureDetails_BBL&WBBL.txt")
+ball_venue_df <- read.delim("BallByBall_VenueDetails_BBL&WBBL.txt")
+ovals_facility_df <- read.delim("OvalsAndFacility.txt")
+ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
 # #large data
 # deliveries_df <- fread("Deliveries.txt")
+#fix oval facility column names
+colnames(ovals_facility_df) <- c('PropertyID','PropertyName','PropertySuburb',
+                                 'PropertyRegion','FederalElectorateDescription',
+                                 'StateElectorateDescription','NumberOfOvals',
+                                 'PropertyState','PropertyPostcode',
+                                 'PropertyLatitude','PropertyLongitude',
+                                 'OvalID','MyCricketOvalID','OvalName',
+                                 'OvalLatitude','OvalLongitude',
+                                 'OvalDimension','PitchType')
 
+colnames(ovals_facility_audit_df) <- c('AuditID ','PropertyID','FacilityGroup',
+                                       'AuditSectionName','QuestionID','AuditQuestion',
+                                       'QuestionOrder','FacilityNumber','AuditResponse',
+                                       'MeetsNationalCompliance')
 
-
-
-#matches_df <- read.delim("Matches.txt")
-# ovals_facility_df <- read.delim("OvalsAndFacility.txt")
-# ovals_facility_audit_df <- read.delim("OvalsAndFacilityAudit.txt")
-# 
-# #fix column names
-# colnames(ovals_facility_df) <- c('PropertyID','PropertyName','PropertySuburb',
-#                                  'PropertyRegion','FederalElectorateDescription',
-#                                  'StateElectorateDescription','NumberOfOvals',
-#                                  'PropertyState','PropertyPostcode',
-#                                  'PropertyLatitude','PropertyLongitude',
-#                                  'OvalID','MyCricketOvalID','OvalName',
-#                                  'OvalLatitude','OvalLongitude',
-#                                  'OvalDimension','PitchType')
-# 
-# colnames(ovals_facility_audit_df) <- c('AuditID ','PropertyID','FacilityGroup',
-#                                        'AuditSectionName','QuestionID','AuditQuestion',
-#                                        'QuestionOrder','FacilityNumber','AuditResponse',
-#                                        'MeetsNationalCompliance')
-
+#===================
+# Player analytics
+#===================
 
 
 
