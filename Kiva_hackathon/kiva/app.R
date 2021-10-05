de8c9803511442b6ddba8ded9a62b7d2fe0c3f97
Edima <edimaudo@gmail.com>
2021-10-04 21:02:13 -0400

    Create app.R

---
+++ Kiva_hackathon/kiva/app.R
@@ -0,0 +1,17 @@
+# Kiva Application
+
+# Clear environment
+rm(list = ls())
+
+#=============
+# Package Information
+#=============
+packages <- c('ggplot2', 'corrplot','tidyverse','readxl',
+              'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
+              'forecast','TTR','xts','lubridate')
+for (package in packages) {
+    if (!require(package, character.only=T, quietly=T)) {
+        install.packages(package)
+        library(package, character.only=T)
+    }
+}
