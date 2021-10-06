bc84b8afa4c5b1d6265e255bb4ce4247311917e3
Edima <edimaudo@gmail.com>
2021-10-06 19:21:01 -0400

    Update app.R

--- Kiva_hackathon/kiva/app.R
+++ Kiva_hackathon/kiva/app.R
@@ -1,27 +1,23 @@
 #=============
 # Kiva Application
 #=============
-
 rm(list = ls()) # Clear environment
-
 #=============
 # Package Information
 #=============
 packages <- c('ggplot2', 'corrplot','tidyverse','readxl',
               'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
-              'forecast','TTR','xts','lubridate',
-              "dummies",'readxl','forecast','TTR','xts','lubridate')
+              "dummies",'readxl','forecast','TTR','xts','lubridate','data.table')
 for (package in packages) {
     if (!require(package, character.only=T, quietly=T)) {
         install.packages(package)
         library(package, character.only=T)
     }
 }
-
 #=============
-# UI 
+# data
 #=============
-
+df <- data.table::fread("loans.csv")
 #=============
 # UI drop-down
 #=============
@@ -32,70 +28,27 @@ repayment_interval <- c(sort(unique(loans$REPAYMENT_INTERVAL)))
 #=============
 # UI Layout 
 #=============
-ui <- dashboardPage(
-  dashboardHeader(title = "Patient Forecast"),
+ui <- dashboardPage(skin = "green",
+  dashboardHeader(title = "Kiva Application"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Data", tabName = "data", icon = icon("th")),
-      menuItem("Analysis", tabName = "analysis", icon = icon("th")),
-      menuItem("Forecasting", tabName = "Forecast", icon = icon("th"))
+      menuItem("About", tabName = "about", icon = icon("th")),
+      menuItem("Data Insights", tabName = "analysis", icon = icon("th")),
+      menuItem("Fund analysis", tabName = "fund", icon = icon("th")),
+      menuItem("Loan Impact", tabName = "loan", icon = icon("th"))
     )
   ),
   dashboardBody(
-    tabItems(
-      tabPanel("Introduction",
-               includeMarkdown("intro.md"),
+      tabPanel("about",
+               includeMarkdown("about.md"),
                hr()),
-      tabItem(tabName = "data",
-              sidebarLayout(
-                sidebarPanel(
-                  fileInput("file1", "Choose CSV File", accept = ".csv"),
-                  checkboxInput("header", "Header", TRUE)
-                ),
-                mainPanel(
-                  tableOutput("contents")
-                )
-              )
-      ),
-      tabItem(tabName = "analysis",
-              sidebarLayout(
-                sidebarPanel(
-                  selectInput("aggregateInput", "Aggregate", 
-                              choices = aggregate_info, selected = 'daily'),
-                  selectInput("frequencyInput", "Frequency", 
-                              choices = frequency_info, selected = 7),
-                  radioButtons("differenceInput","Difference",
-                               choices = difference_info, selected = "No"),
-                  numericInput("differenceNumericInput", "Difference Input", 
-                               1, min = 1, max = 52, step = 0.5),
-                  radioButtons("logInput","Log",
-                               choices = log_info, selected = "No"),
-                  submitButton("Submit")
-                ),
-                mainPanel(
-                  h1("Analysis",style="text-align: center;"), 
-                  tabsetPanel(type = "tabs",
-                              tabPanel(
-                                h4("Decomposition",
-                                   style="text-align: center;"),
-                                plotOutput("decompositionPlot")),
-                              tabPanel(
-                                h4("Multi seasonal Decomposition",
-                                   style="text-align: center;"),
-                                plotOutput("multidecompositionPlot")),
-                              tabPanel(
-                                h4("ACF Plot",style="text-align: center;"), 
-                                plotOutput("acfPlot")),
-                              tabPanel(
-                                h4("PACF Plot",style="text-align: center;"), 
-                                plotOutput("pacfPlot"))
-                  )
-                )
-              )  
+      tabItems(
+        
       )
-    )
-  )
 )
+
+)
+
 
 #=============
 # Define server logic 
@@ -118,10 +71,11 @@ shinyApp(ui, server)
 
 #===============
 #TO DO
-#- BUILD preliminary UI LAYOUT
 #- BUILD ABOUT
-#- BUILD UI LAYOUT
 #- LOAD DATA + INFO
+#- BUILD preliminary UI LAYOUT
+
+#- BUILD UI LAYOUT
 # - Fund distribution - How might we optimize fund distribution to borrowers?
 # - Loan impact - How might we show the impact of the loans?
 # - Test model
