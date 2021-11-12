915cdbe38501cd808e7fcb34a7446b05191a5672
Edima <edimaudo@gmail.com>
2021-11-12 17:43:13 -0500

    Update app.py

--- modzy_ai_hackathon/app.py
+++ modzy_ai_hackathon/app.py
@@ -4,6 +4,7 @@
 # Also uses plotly to generate bar charts
 #================
 
+
 # Load libraries
 import streamlit as st
 import pandas as pd
@@ -20,15 +21,15 @@ def load_data():
 	data = pd.read_excel(DATA_URL)
 	return data
 
+
 # Load data
 DATA_URL = "otf.xlsx"
 df = load_data()
 df_backup = df
 INPUT_TEXT = "test.txt"
 
-#=====================
+
 # Text Description data
-#=====================
 english_info_df = df[['Organization_name','English_description']]
 english_info_df = english_info_df.groupby(['Organization_name', 'English_description']).first()
 
@@ -42,7 +43,6 @@ english_info_df = english_info_df.groupby(['Organization_name', 'English_descrip
 #=================
 # Dropdowns values
 #=================
-
 # Geographical area 
 geographical_area_info = df['Recipient_org_city_update'].unique()
 geographical_area_info = geographical_area_info.astype('str')
@@ -64,11 +64,11 @@ submit_checkbox = st.sidebar.checkbox('UPDATE')
 
 
 #==================
-# Updated dataframe based on selection
+# Submit logic
 #==================
 if submit_checkbox:
     if geo_area_selectbox != "All":
-        df = df[(df['Fiscal_year_update'] <= fiscal_year_slider ) & (df['Recipient_org_city_update'] == geo_area_selectbox) ]
+        df = df[(df['Fiscal_year_update'] <= fiscal_year_slider ) & (df['Recipient_org_city_update'] == geo_area_selectbox)]
     else:
         df = df[(df['Fiscal_year_update'] <= fiscal_year_slider)]
     english_description_info = df['English_description'].unique() #generate text
@@ -77,8 +77,6 @@ if submit_checkbox:
     english_description_info.to_csv("input.txt")
 else:
     df = df_backup
-
-
 
 #================
 # Metrics logic
@@ -96,8 +94,6 @@ org_count = df_metrics_org_agg['Organization Name'].count()
 amount_awarded = df_metrics_org_agg['Total Amount Awarded'].sum()
 amount_awarded = "{:,}".format(amount_awarded)
 
-
-
 #==================
 # Metrics setup
 #==================
@@ -106,6 +102,69 @@ metric_col1, metric_col2, metric_col3 = st.columns(3)
 metric_col1.metric("# of organizations", str(geo_count))
 metric_col2.metric("Areas Served", str(org_count))
 metric_col3.metric("Amount Awarded (CAD)", str(amount_awarded))
+
+#================
+# Text analytics logic
+#================
+API_URL = "https://app.modzy.com/api"
+API_KEY = "81RXRBBjPDUaGDuCrC38.ZNGC6q7LmLhtoIiPwTiT"
+
+# setup our API Client
+client = ApiClient(base_url=API_URL, api_key=API_KEY)
+
+# Sentiment analysis model
+sentiment_model_info = client.models.get_by_name("Sentiment Analysis")
+
+# Topic modelling model
+topic_model_info = client.models.get_by_name("Text Topic Modeling")
+
+# function to flatten JSON
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+def sentiment_analysis(input_text):
+    job = client.jobs.submit_text('ed542963de', '1.0.1', {'input.txt': input_text})
+    result = client.results.block_until_complete(job, timeout=None)
+    return (result['results']['job']['results.json']['data']['result'])
+
+#json_output_df = st.dataframe("")
+if submit_checkbox:
+    json_output = sentiment_analysis(INPUT_TEXT)
+    json_output_flat = flatten_json(json_output)
+    json_output_df = json_normalize(json_output_flat)
+    json_output_df = st.dataframe(json_output_df)
+
+# def topic_analysis(input_text):
+#     job = client.jobs.submit_text('m8z2mwe3pt', '1.0.1', {'input.txt': input_text})
+#     result = client.results.block_until_complete(job, timeout=None)
+#     return (result['results']['job']['results.json'])
+
+
+#================
+# Text analytics setup
+#================
+text_analysis_column1, text_analysis_column2, text_analysis_column3 = st.columns(3)
+text_analysis_column1.header("Topic Modeling")
+if submit_checkbox:
+    text_analysis_column1.dataframe(json_output_df)
+text_analysis_column2.header("Sentiment analysis")
+text_analysis_column3.header("Named Entity Recognition")
+
 
 
 #==================
@@ -151,7 +210,8 @@ metric_col3.metric("Amount Awarded (CAD)", str(amount_awarded))
 #                                                                       ('Amount_awarded', 'sum')).reset_index()
 # df_total_grants_agg.columns = ['Program Area','Fiscal Year', 'Total Amount Awarded']
 # if not df_total_grants_agg.empty:
-# 	grant_program_fig = px.bar(df_total_grants_agg, x="Fiscal Year", y="Total Amount Awarded",color='Program Area',height=400)
+# 	grant_program_fig = px.bar(df_total_grants_agg, x="Fiscal Year", y="Total Amount Awarded",
+# color='Program Area',height=400)
 # else:
 # 	grant_program_fig = ""
 # ## Total grants by year by age group
@@ -175,54 +235,7 @@ metric_col3.metric("Amount Awarded (CAD)", str(amount_awarded))
 
 # # Total grants by Grant Program by year
 
-#================
-# Text analytics logic
-#================
-# API_URL = "https://app.modzy.com/api"
-# API_KEY = "81RXRBBjPDUaGDuCrC38.ZNGC6q7LmLhtoIiPwTiT"
 
-# # setup our API Client
-# client = ApiClient(base_url=API_URL, api_key=API_KEY)
-
-# # Sentiment analysis model
-# sentiment_model_info = client.models.get_by_name("Sentiment Analysis")
-
-# # Topic modelling model
-# topic_model_info = client.models.get_by_name("Text Topic Modeling")
-
-# # function to flatten JSON
-# def flatten_json(y):
-#     out = {}
-
-#     def flatten(x, name=''):
-#         if type(x) is dict:
-#             for a in x:
-#                 flatten(x[a], name + a + '_')
-#         elif type(x) is list:
-#             i = 0
-#             for a in x:
-#                 flatten(a, name + str(i) + '_')
-#                 i += 1
-#         else:
-#             out[name[:-1]] = x
-
-#     flatten(y)
-#     return out
-
-# def sentiment_analysis(input_text):
-#     job = client.jobs.submit_text('ed542963de', '1.0.1', {'input.txt': input_text})
-#     result = client.results.block_until_complete(job, timeout=None)
-#     return (result['results']['job']['results.json']['data']['result'])
-
-# json_output = sentiment_analysis(INPUT_TEXT)
-# json_output_flat = flatten_json(json_output)
-# json_output_df = json_normalize(json_output_flat)
-# st.dataframe(json_output_df)
-
-# def topic_analysis(input_text):
-#     job = client.jobs.submit_text('m8z2mwe3pt', '1.0.1', {'input.txt': input_text})
-#     result = client.results.block_until_complete(job, timeout=None)
-#     return (result['results']['job']['results.json'])
 
 
 
@@ -255,12 +268,5 @@ metric_col3.metric("Amount Awarded (CAD)", str(amount_awarded))
 # 	col2.plotly_chart(grant_budget_fig)
 
 
-#================
-# Text analytics setup
-#================
-# column1, column2, column3 = st.columns(3)
-# column1.header("Topic Modeling")
-# column2.header("Sentiment analysis")
-# column3.header("Named Entity Recognition")
 
-#st.dataframe(df)
+
