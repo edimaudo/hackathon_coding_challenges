270ccdb30c2c60bb44edccc1a25fe91ec9306010
Edima <edimaudo@gmail.com>
2022-01-26 20:51:23 -0500

    Update

--- nanostring_spatial_omics_hackathon/KidneyDatasetR/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function( libname , pkgname )
-{
-  utils::data( kidney_norm , package = pkgname )
-  utils::data( kidney_target , package = pkgname )
-  utils::data( kidney_probe , package = pkgname )
-}
