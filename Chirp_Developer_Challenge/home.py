5af9e52faad25d258162e24509c34ca44f82988f
Edima <edimaudo@gmail.com>
2022-07-10 20:45:19 -0400

    Update home.py

--- Chirp_Developer_Challenge/home.py
+++ Chirp_Developer_Challenge/home.py
@@ -10,7 +10,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load Twitter data
-cities = ['Ajax','Aurora','Bradford West Gwillimbury','Brampton','Brock','Burlington','Caledon','Clarington','East Gwillimbury','Georgina','Halton Hills','King','Markham','Milton','Mississauga','Mono','New Tecumseth','Newmarket','Oakville','Orangeville','Oshawa','Pickering','Richmond Hill','Scugog','Uxbridge','Vaughan','Whitby','Whitchurch-Stouffville']
+cities = ['Ajax','Aurora','Bradford West Gwillimbury','Brampton','Brock','Burlington','Caledon',
+'Clarington','East Gwillimbury','Georgina','Halton Hills','King','Markham','Milton','Mississauga',
+'Mono','New Tecumseth','Newmarket','Oakville','Orangeville','Oshawa','Pickering','Richmond Hill',
+'Scugog','Uxbridge','Vaughan','Whitby','Whitchurch-Stouffville']
 
 
 st.title('Overview')
@@ -20,18 +23,17 @@ options = st.multiselect(
 
  #filter data    
 
-
-
 st.header("Twitter Metrics")
 metric_column1, metric_column2= st.columns(2)
 metric_column1("Tweets today","")
 metric_column2("Tweeters today","")
 
-
+st.header("Twitter Graphs")
     # Graphs 
     #Tweet volume (today, week)
     #tweets by hour of day (today,week)
 
+st.header("Top of the Tweets")
     # Container
     #twitter grid design
     #Most liked tweets (past 12 hours)
