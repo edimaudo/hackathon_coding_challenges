80fff15bdc3360900711ab605e8fd66f24b731db
Edima <edimaudo@gmail.com>
2022-07-13 05:09:13 -0400

    Update About.py

--- Chirp_Developer_Challenge/pages/About.py
+++ Chirp_Developer_Challenge/pages/About.py
@@ -1,2 +1,5 @@
  st.title('About')
- st.text('GTAEhTweets was built as part of the Chirp Developer Challenge.  The main tools used were streamlit and Twitter API')
+ st.write('GTAEhTweets was built as part of the 2022 Chirp Developer Challenge. ')
+ st.write("The goal is to build a tool that would analyze data from the Greater Toronto Area to understand what's happening.") 
+ st.write('The main tools used were streamlit and Twitter API')
+
