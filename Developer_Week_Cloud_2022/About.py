6fbfa85deec0be79a17ca3438b891ba263e6d1dc
Edima <edimaudo@gmail.com>
2022-08-10 08:47:29 -0400

    Delete About.py

--- Developer_Week_Cloud_2022/About.py
+++
@@ -1,9 +0,0 @@
-# Load libraries
-import streamlit as st
-
-st.title('OTF Charity Insights')
-st.write("The goal is to use open data from Ontario Trillium Foundation to analyze charity information")
-st.write("The Ontario Trillium Foundation (OTF) is an agency of the Government of Ontario and one of Canada’s leading granting foundations. Our investments in communities across the province help build healthy and vibrant communities. Our key funder, the Ministry of Heritage, Sport, Tourism and Culture Industries enables us to provide grants that can make the greatest impact. OTF also administers grants on behalf of the Ministry of Children, Community and Social Services. Last year, $115 million was invested into more than 644 projects in communities across the province")
-st.write("https://otf.ca/who-we-are/about-us/our-story")
-
-
