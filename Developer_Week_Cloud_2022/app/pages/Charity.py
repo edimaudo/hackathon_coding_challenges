f6bd313ac983e4d8a49bd8fd81e7e57b85531684
Edima <edimaudo@gmail.com>
2022-08-11 11:56:27 -0400

    Create Charity.py

---
+++ Developer_Week_Cloud_2022/app/pages/Charity.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import os, os.path
+import pandas as pd
+
+st.header("Charity Insights")
+
+#dropdown field
+charity_choice = st.selectbox("")
