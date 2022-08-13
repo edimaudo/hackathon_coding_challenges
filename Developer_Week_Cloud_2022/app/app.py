fc85d1aaaed549497ca2ffaff38cee8e826d3626
Edima <edimaudo@gmail.com>
2022-08-13 11:51:38 -0400

    Update app.py
    Completed Charity Insights - Charity Overview section

--- Developer_Week_Cloud_2022/app/app.py
+++ Developer_Week_Cloud_2022/app/app.py
@@ -4,9 +4,17 @@ import pandas as pd
 import plotly.express as px
 import os, os.path
 
+@st.cache
+def load_data():
+    data = pd.read_excel(DATA_URL)
+    return data
+
+# Load data
+DATA_URL = "otf.xlsx"
+df = load_data()
+
 st.title('OTF Charity Insights')
-
-
+# About
 st.header("About")
 with st.expander("About"):
     st.image("trillium-logo.jpeg")
@@ -18,17 +26,8 @@ with st.expander("About"):
     "644 projects in communities across the province")
     st.write("https://otf.ca/who-we-are/about-us/our-story")
 
-@st.cache
-def load_data():
-    data = pd.read_excel(DATA_URL)
-    return data
-
-# Load data
-DATA_URL = "otf.xlsx"
-df = load_data()
-
+# Overview
 st.header("Overview")
-
 with st.expander("Overview"):
     top_container = st.container()
     metric_column1, metric_column2,metric_column3,metric_column4, metric_column5,metric_column6 = st.columns(6)
@@ -39,8 +38,7 @@ with st.expander("Overview"):
         metric_column4.metric("Grant Programs",str(len(df['Grant_program'].unique())))
         metric_column5.metric("Program Areas",str(len(df['Program_area_update'].unique())))
         metric_column6.metric("Age Groups",str(len(df['Age_group_update'].unique())))#
-
-    # Funding Trends
+    # Funding Trend
     df_total_grants = df[['Amount_awarded','Fiscal_year_update']]
     df_total_grants_agg = df_total_grants.groupby('Fiscal_year_update').agg(Total_Amount_Awarded = 
                                                                         ('Amount_awarded', 'sum')).reset_index()
@@ -51,9 +49,7 @@ with st.expander("Overview"):
 
 # Charity Insights
 st.header("Charity Insights")
-charity_top_container = st.container()
-charity_bottom_container = st.container()
-charity_metric_column1, charity_metric_column2,charity_metric_column3,charity_metric_column4 = st.columns(4)
+
 with st.expander("Charity Insights"):
     charity_list = df['Organization_name'].unique()
     charity_list  = charity_list.astype('str')
@@ -61,17 +57,42 @@ with st.expander("Charity Insights"):
     charity_choice = st.selectbox("Pick a Charity",charity_list)
 
     st.subheader("Charity Overview")
-    charity = df[(df.Organization_name == charity_choice)]
-    ##English description
-    ##incorporation #
-    ##registration #
-    ##Number of cities
-    ##Funding Trend
+    charity_container = st.container()
+
+   
+    with charity_container:
+        charity_metric_column1, charity_metric_column2,charity_metric_column3,charity_metric_column4 = st.columns(4)
+        charity = df[(df.Organization_name == charity_choice)]
+        # English description
+        charity_name = charity['Organization_name'].unique()
+        # Incorporation #
+        charity_incorporation = charity['Recipient_org_incorporation_number'].unique()
+        # registration #
+        charity_registration = charity['Recipient_org_registration_number'].unique()
+        # Number of cities
+        charity_cities = len(charity['Organization_name'].unique())
+        charity_metric_column1.subheader("Name")
+        charity_metric_column1.write(str(charity_name[0]))
+        charity_metric_column2.subheader("Registration #")
+        charity_metric_column2.write(str(charity_registration[0]))
+        charity_metric_column3.subheader("# of Cities")
+        charity_metric_column3.write(str(charity_cities))
+        
+        charity_grants = charity[['Amount_awarded','Fiscal_year_update']]
+        charity_grants = charity_grants.groupby('Fiscal_year_update').agg(Total_Amount_Awarded = 
+                                                                        ('Amount_awarded', 'sum')).reset_index()
+        charity_grants.columns = ['Year', 'Amount Awarded (CAD)']
+        charity_grants.sort_values("Amount Awarded (CAD)", ascending=True)
+        fig = px.bar(charity_grants, x="Year", y="Amount Awarded (CAD)")
+        st.plotly_chart(fig)
+
     st.subheader("Yearly Insights")
     year_list = df['Fiscal_year_update'].unique()
     year_list  = year_list.astype('int')
     year_list.sort()
     year_choice = st.selectbox("year",year_list)
+    charity_year = df[(df.Organization_name == charity_choice)& (df.Fiscal_year_update == year_choice)]
+    #newdf = df[(df.origin == "JFK") & (df.carrier == "B6")]
     ##Age breakdown
     ##population served
     ##grant programs
@@ -81,3 +102,9 @@ with st.expander("Charity Insights"):
     ##area served map (https://stackoverflow.com/questions/58043978/display-data-on-real-map-based-on-postal-code)
 
 # City Insights
+
+
+# Prediction
+
+
+# NLP
