32c67b1a363f4fb8f70f458241564026e5d2776f
Edima <edimaudo@gmail.com>
2022-08-24 13:00:31 -0400

    Update app.py

--- Developer_Week_Cloud_2022/app/app.py
+++ Developer_Week_Cloud_2022/app/app.py
@@ -185,16 +185,16 @@ with st.expander("Charity Prediction"):
     budget_fund  = budget_fund.astype('str')
     budget_fund.sort()
 
-    with charity_metric_column1:
-        city_choice = st.selectbox("Pick a City",city)
-    with charity_metric_column2:
-        grant_choice = st.selectbox("Pick a Grant Option",grant)
-    with charity_metric_column3:
-        program_area_choice = st.selectbox("Pick a Program Area",program_area)
-    with charity_metric_column4:
-        age_choice = st.selectbox("Pick an Age group",age)
-    with charity_metric_column5:
-        budget_fund_choice = st.selectbox("Pick a Budget Fund",budget_fund)
+    #with charity_metric_column1:
+    city_choice = st.selectbox("Pick a City",city)
+    #with charity_metric_column2:
+    grant_choice = st.selectbox("Pick a Grant Option",grant)
+    #with charity_metric_column3:
+    program_area_choice = st.selectbox("Pick a Program Area",program_area)
+    #with charity_metric_column4:
+    age_choice = st.selectbox("Pick an Age group",age)
+    #with charity_metric_column5:
+    budget_fund_choice = st.selectbox("Pick a Budget Fund",budget_fund)
     
 
     # Regression Model
