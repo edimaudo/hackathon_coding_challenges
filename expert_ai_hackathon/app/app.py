ce187a30b2a79a1bd512117a89037439db33a83a
Edima <edimaudo@gmail.com>
2022-09-04 17:08:13 -0400

    Update app.py
    Updated about section
    Added summary section code

--- expert_ai_hackathon/app/app.py
+++ expert_ai_hackathon/app/app.py
@@ -20,8 +20,8 @@ st.title('Google Play Printer Insights')
 st.header("About")
 with st.expander("About"):
     st.write("Printers! We all have a love-hate relationship with them.  When things are going well it is perfect.  Just one glitch or driver issue and all hell breaks lose")
-    st.write("There has been a proliferation of printer apps to make the printing process easier.")
-    st.write("The goal is analyze printer app information scraped from the Google Play store")
+    st.write("There has been a proliferation of printer companion apps to make the printing process easier.  These apps are what someone might use to print remotely or scan on the go")
+    st.write("The goal is analyze printer app information scraped from the Google Play store.  The apps analyzed are Epson SmartPanel, Epson iPrint, HP Smart and Canon Print")
 
 # Overview
 st.header("Overview")
@@ -31,7 +31,11 @@ with st.expander("overview"):
 
 st.header("Summary")
 with st.expander("Data summary"):
-    st.write("Overview of the data")
+    metric_column1, metric_column2,metric_column3,metric_column4, metric_column5,metric_column6 = st.columns(6)
+    metric_column1.metric("No. of apps",str( len(df['appId'].unique())))
+    metric_column2.metric("No. of reviewers",str(len(df['userName'].unique())))
+    metric_column3.metric("No. of reviews",str(len(df['reviewId'].unique())))
+    metric_column4.metric("Average Score",str(float("{:.2f}".format(df['score'].mean()))))
     
 
 # Analysis
