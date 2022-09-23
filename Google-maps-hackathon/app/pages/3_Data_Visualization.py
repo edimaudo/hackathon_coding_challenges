325e80724356eba2fddbc7b97b89ddc3640730e2
Edima <edimaudo@gmail.com>
2022-09-23 17:53:04 -0400

    Update 3_Data_Visualization.py
    Added
    grant programme, budget fund, city and area served

--- Google-maps-hackathon/app/pages/3_Data_Visualization.py
+++ Google-maps-hackathon/app/pages/3_Data_Visualization.py
@@ -45,3 +45,43 @@ with st.expander(" "):
     population_served_agg = population_served_agg.sort_values("Amount Awarded (CAD)", ascending=True).reset_index()
     fig = px.bar(population_served_agg, x="Amount Awarded (CAD)", y="Population Served", orientation='h')
     st.plotly_chart(fig)
+
+    # Grant programs
+    st.subheader("Grant Programme")
+    grant_program = charity_year[['Amount Awarded','Grant Programme']]
+    grant_program_agg = grant_program.groupby('Grant Programme').agg(Total_Amount_Awarded = 
+                                                                        ('Amount Awarded', 'sum')).reset_index()
+    grant_program_agg.columns = ['Grant Programme', 'Amount Awarded (CAD)']
+    grant_program_agg = grant_program_agg.sort_values("Amount Awarded (CAD)", ascending=True).reset_index()
+    fig = px.bar(grant_program_agg, x="Amount Awarded (CAD)", y="Grant Programme", orientation='h')
+    st.plotly_chart(fig)
+
+    # budget fund
+    st.subheader("Budget Fund")
+    budget_fund = charity_year[['Amount Awarded','Budget Fund']]
+    budget_fund_agg = budget_fund.groupby('Budget Fund').agg(Total_Amount_Awarded = 
+                                                                        ('Amount Awarded', 'sum')).reset_index()
+    budget_fund_agg.columns = ['Budget Fund', 'Amount Awarded (CAD)']
+    budget_fund_agg = budget_fund_agg.sort_values("Amount Awarded (CAD)", ascending=True).reset_index()
+    fig = px.bar(budget_fund_agg, x="Amount Awarded (CAD)", y="Budget Fund", orientation='h')
+    st.plotly_chart(fig)
+
+    # Cities
+    st.subheader("Cities")
+    recipient_org_city_update = charity_year[['Amount Awarded','City']]
+    recipient_org_city_update_agg = recipient_org_city_update.groupby('City').agg(Total_Amount_Awarded = 
+                                                                        ('Amount Awarded', 'sum')).reset_index()
+    recipient_org_city_update_agg.columns = ['City', 'Amount Awarded (CAD)']
+    recipient_org_city_update_agg = recipient_org_city_update_agg.sort_values("Amount Awarded (CAD)", ascending=True).reset_index()
+    fig = px.bar(recipient_org_city_update_agg, x="Amount Awarded (CAD)", y="City", orientation='h')
+    st.plotly_chart(fig) 
+
+    # Geographical Area Served
+    St.subheader("Geographical area served")
+    geo_area_update = charity_year[['Amount Awarded','Geographical Area Served']]
+    geo_area_update_agg = geo_area_update.groupby('Geographical Area Served').agg(Total_Amount_Awarded = 
+                                                                        ('Amount Awarded', 'sum')).reset_index()
+    geo_area_update_agg.columns = ['Geographical Area Served', 'Amount Awarded (CAD)']
+    geo_area_update_agg = geo_area_update.sort_values("Amount Awarded (CAD)", ascending=True).reset_index()
+    fig = px.bar(geo_area_update_agg, x="Amount Awarded (CAD)", y="Geographical Area Served", orientation='h')
+    st.plotly_chart(fig) 
