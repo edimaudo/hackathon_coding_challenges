0b0d5c5cc0b2191a0af6f19f2218f8e50cb685aa
Edima <edimaudo@gmail.com>
2022-09-23 17:09:40 -0400

    Update 3_Data_Visualization.py
    Added  Year dropdown

--- Google-maps-hackathon/app/pages/3_Data_Visualization.py
+++ Google-maps-hackathon/app/pages/3_Data_Visualization.py
@@ -12,7 +12,8 @@ with st.expander(" "):
     year_list = df['Fiscal Year'].unique()
     year_list  = year_list.astype('int')
     year_list.sort()
-    year_choice = st.selectbox("year",year_list)
+    year_choice = st.selectbox("Year",year_list)
+    charity_year = df[(df['Fiscal Year'] == year_choice)]
 
     age_group = charity_year[['Amount Awarded','Age Group']]
     age_group_agg = age_group.groupby('Age Group').agg(Total_Amount_Awarded = 
