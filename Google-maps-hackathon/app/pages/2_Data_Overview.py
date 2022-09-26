a4a26daa643c0744d7137abcc7106bf2dec83bf3
Edima <edimaudo@gmail.com>
2022-09-26 12:50:27 -0400

    Update 2_Data_Overview.py

--- Google-maps-hackathon/app/pages/2_Data_Overview.py
+++ Google-maps-hackathon/app/pages/2_Data_Overview.py
@@ -4,7 +4,14 @@ import pandas as pd
 import plotly.express as px
 
 st.title('OTF Charity Insights')
-df = st.session_state['df']
+@st.cache
+def load_data():
+    data = pd.read_excel(DATA_URL)
+    return data
+
+# Load data
+DATA_URL = "OTF.xlsx"
+df = load_data()
 # Data preview
 st.header("Data Preview")
 with st.expander(" "):
@@ -30,3 +37,4 @@ df_total_grants_agg.sort_values("Amount Awarded (CAD)", ascending=True)
 fig = px.bar(df_total_grants_agg, x="Year", y="Amount Awarded (CAD)")
 st.plotly_chart(fig)
 
+# Add map for funding
