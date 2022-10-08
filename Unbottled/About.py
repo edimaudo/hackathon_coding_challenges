3de57008ac5de85a8d53812c9a5c566e3bc57279
Edima <edimaudo@gmail.com>
2022-10-08 16:09:05 -0400

    Create About.py

---
+++ Unbottled/About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('Unbottled')
+st.write(""" 
+Unbottled is a wine insights & exploration app.  provides insights into prices, ratings and geographic distribution of the world's most esteemed wines. Novice or connoisseur, consumer or seller, this app will meet your oenophile needs.
+""")
