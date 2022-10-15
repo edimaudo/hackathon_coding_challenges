53f49cf8a808d1d8820a861dd3f918ecc3987c8e
Edima <edimaudo@gmail.com>
2022-10-15 16:36:14 -0400

    Update Data_Overview.py

--- Civicode/kiva/Data_Overview.py
+++ Civicode/kiva/Data_Overview.py
@@ -1,12 +1,9 @@
 # Libraries
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import os, os.path
-import sklearn
 
 st.title('Kiva Insights')
-
 # Load data
 @st.cache
 def load_data():
@@ -29,7 +26,21 @@ with st.expander(" "):
     st.dataframe(df.head(35))
 
 st.header("Data Summary")
-
+top_container = st.container()
+middle_container = st.container()
+bottom_container = st.container()
+with top_container:
+    metric_column1, metric_column2,metric_column3 = st.columns(3)
+    with metric_column1:
+        st.metric("","")
+#STATUS
+#ORIGINAL_LANGUAGE
+#LOAN_AMOUNT #avg
+#SECTOR_NAME
+#COUNTRY_NAME
+#CURRENCY_POLICY
+#LENDER_TERM
+#CURRENCY
 
 
 
