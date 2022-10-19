93b6bd3aa28225d94d201bb2fec31b20e3270009
Edima <edimaudo@gmail.com>
2022-10-18 20:17:15 -0400

    Update 2_Fund_Distribution.py

--- Civicode/kiva/pages/2_Fund_Distribution.py
+++ Civicode/kiva/pages/2_Fund_Distribution.py
@@ -8,6 +8,7 @@ import numpy as np
 from datetime import datetime
 import random
 warnings.simplefilter(action='ignore', category=FutureWarning)
+import math 
 
 st.title('Kiva Insights')
 # Load data
@@ -48,7 +49,7 @@ fund_df_agg_pivot = fund_df_agg_pivot.fillna(0)
 # mean daily loans
 mean_ret = fund_df_agg_pivot.mean(axis=0)
 # covariance matrix with annualization
-cov_mat = np.cov(fund_df_agg_pivot, bias=True) * 252
+cov_mat = pd.DataFrame.cov(fund_df_agg_pivot) * 252
 #simulation of 10000 portfolios
 num_port = 10000
 # Creating a matrix to store the weights
@@ -63,37 +64,50 @@ sharpe_ratio = []
 random.seed(10)
 # Simulation
 sim_data = list(range(1,num_port+1))
-#for value in sim_data:
-wts = np.random.uniform(0,1,sector_count)
-wts = wts/sum(wts)
-# Storing weight
-all_wts.append(wts)
+for value in sim_data:
+    #for value in sim_data:
+    wts = np.random.uniform(0,1,sector_count)
+    wts = wts/sum(wts)
+    # Storing weight
+    all_wts.append(wts)
 
-# Portfolio returns
-port_ret <- sum(wts * mean_ret)
-port_ret = ((port_ret + 1)^252) - 1
+    # Portfolio returns
+    port_ret = sum(wts * mean_ret)
+    #port_ret = ((port_ret + 1)**252) - 1
 
-# Storing Portfolio Returns values
-port_returns.append(port_ret)
+    # Storing Portfolio Returns values
+    port_returns.append(port_ret)
 
-# Creating and storing portfolio risk
-#port_sd <- sqrt(t(wts) %*% (cov_mat  %*% wts))
-#port_risk[i] <- port_sd
+    # Creating and storing portfolio risk
+    port_sd = math.sqrt(np.matmul(wts.transpose(),(np.matmul(cov_mat,wts))))
+    port_risk.append(port_sd)
 
-# Creating and storing Portfolio Sharpe Ratios
-# Assuming 0% Risk free rate
-#sr <- port_ret/port_sd
-#sharpe_ratio.append(sr)
+    # Creating and storing Portfolio Sharpe Ratios
+    # Assuming 0% Risk free rate
+    sr = port_ret/port_sd
+    sharpe_ratio.append(sr)
 
+# Storing the values in the table
+portfolio_values = pd.DataFrame()
+portfolio_values['Return']  = port_returns
+portfolio_values['Risk']  = port_risk
+portfolio_values['SharpeRatio']  = sharpe_ratio
+
+column_info = fund_df_agg['SECTOR NAME'].unique()
+all_weights = pd.DataFrame (all_wts, columns = column_info)
+
+# Combing all the values together
+portfolio_values = pd.concat([all_weights, portfolio_values], axis = 1)
+st.write(portfolio_values)
 
 st.subheader("Minimum Variance Portfolio")
 
-##st.write("""
-##A minimum variance portfolio is an investing method that helps you maximize returns and minimize risk. It involves diversifying your holdings to reduce volatility, or such that investments that may be risky on their own balance each other out when held together.
-##""")
+st.write("""
+A minimum variance portfolio is an investing method that helps you maximize returns and minimize risk. It involves diversifying your holdings to reduce volatility.
+""")
 
 st.subheader("Efficient Portfolio")
 
-##st.write("""
-##investable assets are combined in a way that produces the best possible expected level of return for their level of risk—or the lowest risk for a target return
-##""")
+st.write("""
+investable assets are combined in a way that produces the best possible expected level of return for their level of risk—or the lowest risk for a target return.
+""")
