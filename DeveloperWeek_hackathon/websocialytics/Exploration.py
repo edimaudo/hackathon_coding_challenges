01b9ac4db3295f542f1b178491c649bc171f9663
Edima <edimaudo@gmail.com>
2022-10-30 12:51:11 -0400

    Update Exploration.py

--- DeveloperWeek_hackathon/websocialytics/Exploration.py
+++ DeveloperWeek_hackathon/websocialytics/Exploration.py
@@ -9,19 +9,17 @@ from datetime import datetime
 import random
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import math 
-
+import re, string
 
 st.title('WebSocialytics Insights')
 
 # Load data
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data():
     data = pd.read_csv(DATA_URL)
     return data
 DATA_URL = "CustomerReviews2000.csv"
 df = load_data()
-
-
 
 st.header("About")
 with st.expander(" "):
@@ -39,3 +37,39 @@ making process for analytical scenarios
     """)
 
 st.header('Data Exploration')
+
+nlp_retailer_list = df['RetailerName'].unique()
+nlp_retailer_list  = nlp_retailer_list.astype('str')
+nlp_retailer_list.sort()
+
+nlp_manufacturer_list = df['ManufacturerName'].unique()
+nlp_manufacturer_list = nlp_manufacturer_list.astype('str')
+nlp_manufacturer_list.sort()
+
+nlp_product_cat_list = df['ProductCategory'].unique()
+nlp_product_cat_list  = nlp_product_cat_list.astype('str')
+nlp_product_cat_list.sort()
+
+nlp_retailer_city_list = df['RetailerCity'].unique()
+nlp_retailer_city_list  = nlp_retailer_city_list.astype('str')
+nlp_retailer_city_list.sort()
+
+
+with st.sidebar:
+    nlp_manufacturer_choice = st.multiselect("Manufacturer",nlp_manufacturer_list,['Samsung','Microsoft'])
+    nlp_retailer_choice = st.multiselect("RetailerName",nlp_retailer_list, ['Bestbuy','Walmart'])
+    nlp_city_choice = st.multiselect("City",nlp_retailer_city_list, ['Los Angeles','San Francisco'])
+
+clicked = st.button("Explore")
+if clicked:
+    nlp_analysis = df[(df.ManufacturerName.isin(nlp_manufacturer_choice)) & 
+                (df.RetailerName.isin(nlp_retailer_choice)) & 
+                (df.RetailerCity.isin(nlp_city_choice))]
+
+    #- top 10 products, bottom 10 products by ratings
+    #- top 10 products, bottom 10 products by # of reviews
+    #- top 10 products, bottom 10 products by city
+    # cateogry
+    #- sales trend
+
+
