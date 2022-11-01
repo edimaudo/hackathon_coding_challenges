421308d8afb030b4335508eb37692d90765a151b
Edima <edimaudo@gmail.com>
2022-10-31 20:39:38 -0400

    Update 2_Product_Rating_Classification.py

--- DeveloperWeek_hackathon/websocialytics/pages/2_Product_Rating_Classification.py
+++ DeveloperWeek_hackathon/websocialytics/pages/2_Product_Rating_Classification.py
@@ -1,14 +1,8 @@
-# classification
-#Product Classification
-#How likely a certain product will receive a bad rating in certain zip
-#code
-
 # Libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os, os.path
-import geopandas
 import sklearn
 from sklearn import preprocessing
 from sklearn import model_selection
@@ -19,6 +13,12 @@ from sklearn.linear_model import LinearRegression
 from sklearn import ensemble
 from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor, VotingRegressor
 from sklearn.neighbors import KNeighborsRegressor
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import random
+import pickle
+from pycaret.classification import *
+import datetime
 
 st.title('WebSocialytics Insights')
 
@@ -32,29 +32,69 @@ df = load_data()
 
 st.header("Product Classification")
 
-nlp_retailer_list = df['RetailerName'].unique()
-nlp_retailer_list  = nlp_retailer_list.astype('str')
-nlp_retailer_list.sort()
+model_df = df[['ProductCategory','ProductPrice',
+               'RetailerName','RetailerCity','RetailerState',
+               'ManufacturerName','UserAge','UserGender','UserOccupation',
+               'ReviewDate','ReviewRating']]
+model_df['Month'] = pd.to_datetime(df['ReviewDate']).dt.month_name()
+model_df['Year'] = pd.DatetimeIndex(df['ReviewDate']).year
+model_df = model_df[['ProductCategory','ProductPrice',
+               'RetailerName','RetailerCity','RetailerState',
+               'ManufacturerName','UserAge','UserGender','UserOccupation',
+               'Month','Year','ReviewRating']]
 
-nlp_manufacturer_list = df['ManufacturerName'].unique()
-nlp_manufacturer_list = nlp_manufacturer_list.astype('str')
-nlp_manufacturer_list.sort()
-
-nlp_product_cat_list = df['ProductCategory'].unique()
+nlp_product_cat_list = model_df['ProductCategory'].unique()
 nlp_product_cat_list  = nlp_product_cat_list.astype('str')
 nlp_product_cat_list.sort()
 
-nlp_retailer_city_list = df['RetailerCity'].unique()
+nlp_retailer_list = model_df['RetailerName'].unique()
+nlp_retailer_list  = nlp_retailer_list.astype('str')
+nlp_retailer_list.sort()
+
+nlp_retailer_city_list = model_df['RetailerCity'].unique()
 nlp_retailer_city_list  = nlp_retailer_city_list.astype('str')
 nlp_retailer_city_list.sort()
 
-with st.sidebar:
-    nlp_manufacturer_choice = st.multiselect("Manufacturer",nlp_manufacturer_list,['Samsung','Microsoft'])
-    nlp_retailer_choice = st.multiselect("RetailerName",nlp_retailer_list, ['Bestbuy','Walmart'])
-    nlp_city_choice = st.multiselect("City",nlp_retailer_city_list, ['Los Angeles','San Francisco'])
+nlp_manufacturer_list = model_df['ManufacturerName'].unique()
+nlp_manufacturer_list = nlp_manufacturer_list.astype('str')
+nlp_manufacturer_list.sort()
 
-clicked = st.button("Explore")
-if clicked:
-    nlp_analysis = df[(df.ManufacturerName.isin(nlp_manufacturer_choice)) & 
-                (df.RetailerName.isin(nlp_retailer_choice)) & 
-                (df.RetailerCity.isin(nlp_city_choice))]
+nlp_gender_list = model_df['UserGender'].unique()
+nlp_gender_list = nlp_gender_list.astype('str')
+nlp_gender_list.sort()
+
+nlp_occupation_list = model_df['UserOccupation'].unique()
+nlp_occupation_list = nlp_occupation_list.astype('str')
+nlp_occupation_list.sort()
+
+nlp_month_list = model_df['Month'].unique()
+nlp_month_list = pd.DataFrame(nlp_month_list,columns = ['Month'])
+month_dict = {'January':1,'February':2,'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 
+'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
+nlp_month_list = nlp_month_list.sort_values('Month', key = lambda x : x.apply (lambda x : month_dict[x]))
+
+with st.sidebar:
+    nlp_month_choice = st.selectbox("Month",nlp_month_list)
+    nlp_year_choice = st.selectbox('Year', [2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022])
+    nlp_price_choice = st.slider('Price', 100, 2500, 50)
+    nlp_category_choice = st.selectbox("Category",nlp_product_cat_list)
+    nlp_retailer_choice = st.selectbox("Retailer",nlp_retailer_list)
+    nlp_manufacturer_choice = st.selectbox("Manufacturer",nlp_manufacturer_list)
+    nlp_city_choice = st.selectbox("City",nlp_retailer_city_list)
+    nlp_occupation_choice = st.selectbox("Occupation",nlp_occupation_list)
+    nlp_gender_choice = st.selectbox("Gender",nlp_gender_list)
+
+clicked = st.button("Generate Rating")
+#if clicked:
+    #info_df = pd.DataFrame(columns = ['RevolvingUtilizationOfUnsecuredLines','age','NumberOfTime30-59DaysPastDueNotWorse',
+    #'DebtRatio','MonthlyIncome','NumberOfOpenCreditLinesAndLoans','NumberOfTimes90DaysLate','NumberRealEstateLoansOrLines',
+    #'NumberOfTime60-89DaysPastDueNotWorse','NumberOfDependents'],index = ['a'])
+    #info_df.loc['a'] = [revolving_credit_choice,age_choice,past_due_30_choice,debt_ratio_choice,monthly_income_choice,
+    #open_credit_choice,past_due_90_choice,real_estate_choice,past_due_60_choice,number_dependent_choice]
+    # load model
+    #saved_final_lgbm = load_model('Final LGBM')
+    # Prediction
+    #new_prediction = predict_model(saved_final_lgbm, data=info_df)
+    #score = new_prediction['Score'][0]
+    #st.metric("Users score: ",score)
+    #st.write(" ")
