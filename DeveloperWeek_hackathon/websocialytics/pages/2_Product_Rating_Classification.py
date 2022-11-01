fff9e3651a6f644e80aff503d0bcd74e8b6c9c5b
Edima <edimaudo@gmail.com>
2022-11-01 11:50:14 -0400

    Update 2_Product_Rating_Classification.py

--- DeveloperWeek_hackathon/websocialytics/pages/2_Product_Rating_Classification.py
+++ DeveloperWeek_hackathon/websocialytics/pages/2_Product_Rating_Classification.py
@@ -30,10 +30,9 @@ def load_data():
 DATA_URL = "CustomerReviews2000.csv"
 df = load_data()
 
-st.header("Product Category Ratings")
-st.write("User can predict the predicted rating of a category")
+st.header("Product Ratings Prediction")
 
-model_df = df[['ProductCategory','ProductPrice',
+model_df = df[['ProductModelName','ProductCategory','ProductPrice',
                'RetailerName','RetailerCity','RetailerState',
                'ManufacturerName','UserAge','UserGender','UserOccupation',
                'ReviewDate','ReviewRating']]
@@ -42,6 +41,10 @@ model_df['Month'] = pd.to_datetime(df['ReviewDate']).dt.month_name()
 model_df['Year'] = pd.DatetimeIndex(df['ReviewDate']).year
 
 #drop down
+nlp_product_list = model_df['ProductModelName'].unique()
+nlp_product_list  = nlp_product_list.astype('str')
+nlp_product_list.sort()
+
 nlp_product_cat_list = model_df['ProductCategory'].unique()
 nlp_product_cat_list  = nlp_product_cat_list.astype('str')
 nlp_product_cat_list.sort()
@@ -72,28 +75,10 @@ month_dict = {'January':1,'February':2,'March':3, 'April':4, 'May':5, 'June':6, 
 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
 nlp_month_list = nlp_month_list.sort_values('Month', key = lambda x : x.apply (lambda x : month_dict[x]))
 
-model_data = model_df[['ProductCategory','ProductPrice',
-               'RetailerName','RetailerCity','RetailerState',
-               'ManufacturerName','UserAge','UserGender','UserOccupation',
-               'Month','Year','ReviewRating']]
-# Recode
-model_data["ProductCategory"] = model_data["ProductCategory"].astype('category')
-model_data["RetailerName"] = model_data["RetailerName"].astype('category')
-model_data["RetailerCity"] = model_data["RetailerCity"].astype('category')
-model_data["ManufacturerName"] = model_data["ManufacturerName"].astype('category')
-model_data["UserGender"] = model_data["UserGender"].astype('category')
-model_data["UserOccupation"] = model_data["UserOccupation"].astype('category')
-model_data["Month"] = model_data["Month"].astype('category')
 
-model_data["ProductCategory_cat"] = model_data["ProductCategory"].cat.codes
-model_data["RetailerName_cat"] = model_data["RetailerName"].cat.codes
-model_data["RetailerCity_cat"] = model_data["RetailerCity"].cat.codes
-model_data["ManufacturerName_cat"] = model_data["ManufacturerName"].cat.codes
-model_data["UserGender_cat"] = model_data["UserGender"].cat.codes
-model_data["UserOccupation_cat"] = model_data["UserOccupation"].cat.codes
-model_data["Month_cat"] = model_data["Month"].cat.codes
 
 with st.sidebar:
+    nlp_product_choice = st.selectbox("Product",nlp_product_list)
     nlp_month_choice = st.selectbox("Month",nlp_month_list)
     nlp_year_choice = st.selectbox('Year', [2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022])
     nlp_price_choice = st.slider('Price', 100, 2500, 50)
@@ -107,6 +92,26 @@ with st.sidebar:
 
 clicked = st.button("Predict Rating")
 if clicked:
+    model_data = model_df[['ProductCategory','ProductPrice',
+               'RetailerName','RetailerCity','RetailerState',
+               'ManufacturerName','UserAge','UserGender','UserOccupation',
+               'Month','Year','ReviewRating']]
+    # Recode
+    model_data["ProductCategory"] = model_data["ProductCategory"].astype('category')
+    model_data["RetailerName"] = model_data["RetailerName"].astype('category')
+    model_data["RetailerCity"] = model_data["RetailerCity"].astype('category')
+    model_data["ManufacturerName"] = model_data["ManufacturerName"].astype('category')
+    model_data["UserGender"] = model_data["UserGender"].astype('category')
+    model_data["UserOccupation"] = model_data["UserOccupation"].astype('category')
+    model_data["Month"] = model_data["Month"].astype('category')
+
+    model_data["ProductCategory_cat"] = model_data["ProductCategory"].cat.codes
+    model_data["RetailerName_cat"] = model_data["RetailerName"].cat.codes
+    model_data["RetailerCity_cat"] = model_data["RetailerCity"].cat.codes
+    model_data["ManufacturerName_cat"] = model_data["ManufacturerName"].cat.codes
+    model_data["UserGender_cat"] = model_data["UserGender"].cat.codes
+    model_data["UserOccupation_cat"] = model_data["UserOccupation"].cat.codes
+    model_data["Month_cat"] = model_data["Month"].cat.codes
         
     model_info = model_data[(model_data.ProductCategory == nlp_category_choice)]
     model_info.reset_index(drop=True, inplace=True)
