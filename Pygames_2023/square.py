307953a26ec1f2d42599c7304c8973ed39f7f39f
Edima <edimaudo@gmail.com>
2023-03-25 15:06:41 -0400

    Update square.py
    Added width and height information

--- Pygames_2023/square.py
+++ Pygames_2023/square.py
@@ -12,6 +12,14 @@ from pygame.locals import (
     QUIT,
 )
 
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 pygame.init()
 
 pygame.quit()
