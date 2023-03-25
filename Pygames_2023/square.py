10f5f946e3d7d3aa66e17072c9d55e8d4095822e
Edima <edimaudo@gmail.com>
2023-03-25 15:06:21 -0400

    Update square.py
    Added key information

---
+++ Pygames_2023/square.py
@@ -0,0 +1,17 @@
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+pygame.quit()
