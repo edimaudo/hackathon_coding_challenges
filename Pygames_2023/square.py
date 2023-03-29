bfbfe2a682ef780db714f3cb1270d94b671ed637
Edima <edimaudo@gmail.com>
2023-03-29 04:55:11 -0400

    Update square.py

--- Pygames_2023/square.py
+++ Pygames_2023/square.py
@@ -16,20 +16,29 @@ pygame.display.set_caption("The Square")
 WHITE = (255, 255, 255)
 screen.fill(WHITE)
 
-# Set the dimensions for the black rectangular border
-border_width = 200
-border_height = 150
-border_thickness = 50
-
-# Set the dimensions for the white rectangular block
-block_size = 10
-
 # Set the position of the score text
 score_x = screen_width - 100
 score_y = 50
-
 # Create the score text
 score_text = font.render("Score: 0", True, (0, 0, 0))
 
+# Set up the instructions
+instruction_font = pygame.font.Font(None, 24)
+instruction_color = (0, 0, 0)
+instruction_text = "Use the arrow keys to get the gold coin"
+instruction_x = (screen_width - instruction_font.size(instruction_text)[0]) // 2
+instruction_y = screen_height - instruction_font.size(instruction_text)[1] - 10
+
+# Set the dimensions for the black rectangular border
+border_width = 200
+border_height = 150
+border_thickness = 10
+border_color = (0, 0, 0)
+
+# Set the dimensions for the white rectangular block
+white_box_size = 10
+white_box_color = (255, 255, 255)
+white_box_x = border_thickness
+white_box_y = screen_height - border_height + border_thickness
 
 pygame.quit()
