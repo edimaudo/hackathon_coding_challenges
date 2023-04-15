cf358633223dcf152b6244ba61ff978ff2cb4591
Edima <edimaudo@gmail.com>
2023-04-15 05:51:27 -0400

    Update Overview.py
    Added filters for the overview

--- DeveloperWeek_Europe_2023/mcs/Overview.py
+++ DeveloperWeek_Europe_2023/mcs/Overview.py
@@ -31,15 +31,19 @@ YEAR  = YEAR.astype('int')
 YEAR.sort()
 
 MONTH = df['OCC_MONTH'].unique()
+MONTH  = MONTH.astype('str')
 MONTH = pd.DataFrame(MONTH,columns = ['Month'])
 month_dict = {'January':1,'February':2,'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 
 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
 MONTH = MONTH.sort_values('Month', key = lambda x : x.apply (lambda x : month_dict[x]))
+MONTH = MONTH['Month'].values.tolist()
 
 DAY_OF_WEEK = df['OCC_DOW'].unique()
+DAY_OF_WEEK   = DAY_OF_WEEK.astype('str')
 DAY_OF_WEEK = pd.DataFrame(DAY_OF_WEEK,columns = ['DOW'])
 dow_dict = {'Monday':1,'Tuesday':2,'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday':7}
 DAY_OF_WEEK = DAY_OF_WEEK.sort_values('DOW', key = lambda x : x.apply (lambda x : dow_dict[x]))
+DAY_OF_WEEK = DAY_OF_WEEK['DOW'].values.tolist()
 
 MCI_CATEGORY = df['MCI_CATEGORY'].unique()
 MCI_CATEGORY  = MCI_CATEGORY.astype('str')
@@ -54,21 +58,15 @@ PREMISES_TYPE  = PREMISES_TYPE.astype('str')
 PREMISES_TYPE.sort()
 
 
-with st.container():
-    col1, col2 = st.columns([1, 3])
-    # with col1:
-    #     year_options = st.multiselect('Year',YEAR)
-    # with col2:
-    #     month_options = st.multiselect('Month',MONTH)
-    # with col3:
-    #     dow_options = st.multiselect('Day of Week',DAY_OF_WEEK)
-    # with col4:
-    #     mci_options = st.multiselect('Crime Type',MCI_CATEGORY)
-    # with col5:
-    #     month_options = st.multiselect('Premises Type',PREMISES_TYPE)
-    # with col6:
-    #     month_options = st.multiselect('Neighborhood',NEIGHBORHOOD)
-
+with st.expander("Filters"):
+    year_options = st.multiselect('Year',YEAR,default=YEAR)
+    month_options = st.multiselect('Month',MONTH,default=MONTH)
+    dow_options = st.multiselect('Day of Week',DAY_OF_WEEK,default=DAY_OF_WEEK)
+    mci_options = st.multiselect('Crime Type',MCI_CATEGORY,default=MCI_CATEGORY)
+    premise_options = st.multiselect('Premises Type',PREMISES_TYPE,default =PREMISES_TYPE)
+        
+with container():
+    
 
 
 #TOTAL MCI BY YEAR
