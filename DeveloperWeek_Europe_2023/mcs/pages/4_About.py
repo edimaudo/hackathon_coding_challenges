5e4e0a37661b7d3abb511617d873919a1de18e5f
Edima <edimaudo@gmail.com>
2023-04-15 19:36:28 -0400

    Update 4_About.py

--- DeveloperWeek_Europe_2023/mcs/pages/4_About.py
+++ DeveloperWeek_Europe_2023/mcs/pages/4_About.py
@@ -2,12 +2,12 @@
 import streamlit as st
 
 st.title("Toronto Crime Analysis App")
-st.markdown('This app provides insights into crime trends occuring in Toronto, Canada.  Data is from [Toronto Police Service](https://data.torontopolice.on.ca/pages/major-crime-indicators) and [Toronto Open Data](https://www.toronto.ca/city-government/data-research-maps/open-data/)')
+st.markdown('This app provides insights into crime trends occurring in Toronto, Canada.  Data is from [Toronto Police Service](https://data.torontopolice.on.ca/pages/major-crime-indicators) and [Toronto Open Data](https://www.toronto.ca/city-government/data-research-maps/open-data/)')
 st.markdown("""
 The application has 4 sections:
 - **Crime Overview**
 - **Crime Exploration**
-- **Neighborhood Crime Exploration**
-- **Neighborhood Crime Predictor** 
+- **Neighbourhood Crime Exploration**
+- **Neighbourhood Crime Predictor** 
 """)
 st.markdown('It was inspired by this application [TPS Crime Statistics - Major Crime Indicators](https://data.torontopolice.on.ca/pages/major-crime-indicators)')
