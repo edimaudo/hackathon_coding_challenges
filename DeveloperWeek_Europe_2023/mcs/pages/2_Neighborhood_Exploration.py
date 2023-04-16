54ad3bb31612a485c1d623570826298f54832950
Edima <edimaudo@gmail.com>
2023-04-16 12:48:24 -0400

    Update 2_Neighborhood_Exploration.py

--- DeveloperWeek_Europe_2023/mcs/pages/2_Neighborhood_Exploration.py
+++ DeveloperWeek_Europe_2023/mcs/pages/2_Neighborhood_Exploration.py
@@ -1,14 +1,29 @@
 # Libraries
 from utils import * 
+import streamlit as st
 
 st.title(APP_NAME)
+st.header(NEIGHBOUR_NAME_HEADER)
 
+#Load neighbourhood data
+@st.cache_data
+def load_data(DATA_URL):
+    data = pd.read_csv(DATA_URL)
+    return data
 
-# Year drop down
-# neighboorhood dropdown
-# Month dropdown
-# day of week dropdown
-# crime type dropdown
+DATA_URL = "wellbeing-toronto-economics.csv"    
+neighbourhood_df = load_data(DATA_URL)
+
+with st.expander(APP_FILTERS):
+    neighourhood_options = st.selectbox('Neighbourhood',NEIGHBORHOOD)
+    year_options = st.multiselect('Year',YEAR,default=YEAR)
+    month_options = st.multiselect('Month',MONTH,default=MONTH)
+    dow_options = st.multiselect('Day of Week',DAY_OF_WEEK,default=DAY_OF_WEEK)
+    mci_options = st.multiselect('Crime Type',MCI_CATEGORY,default=MCI_CATEGORY)
+    premises_options = st.multiselect('Premises Type',PREMISES_TYPE,default=PREMISES_TYPE)
+    
+    overview_df = df[(df['OCC_YEAR'].isin(year_options)) & (df['OCC_MONTH'].isin(month_options)) & (df['OCC_DOW'].isin(dow_options)) & 
+    (df['MCI_CATEGORY'].isin(mci_options)) & (df['PREMISES_TYPE'].isin(premises_options))] 
 
 # MCI crime breakdown 
 # premise trend
