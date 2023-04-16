69ddf4b07c4ffa914213d0f73024ec498f36ba7f
Edima <edimaudo@gmail.com>
2023-04-16 09:08:47 -0400

    Update 1_Crime_Exploration.py

--- DeveloperWeek_Europe_2023/mcs/pages/1_Crime_Exploration.py
+++ DeveloperWeek_Europe_2023/mcs/pages/1_Crime_Exploration.py
@@ -2,17 +2,23 @@
 from utils import * 
 
 st.title(APP_NAME)
+st.header(CRIME_NAME_HEADER)
 
-#Year drop down
-#Month dropdown
-#day of week dropdown
-#crime type dropdown
+with st.expander(APP_FILTERS):
+    year_options = st.multiselect('Year',YEAR,default=YEAR)
+    month_options = st.multiselect('Month',MONTH,default=MONTH)
+    dow_options = st.multiselect('Day of Week',DAY_OF_WEEK,default=DAY_OF_WEEK)
+    mci_options = st.multiselect('Crime Type',MCI_CATEGORY,default=MCI_CATEGORY)
+    premises_options = st.multiselect('Premises Type',PREMISES_TYPE,default =PREMISES_TYPE)
+
+with st.container():
 
 #crime breakdown
 # crime trend
+# by Year
 # by month
+# by day of the month 
 # by day of week
 # by hour
-# BY HOUR AND DAY OF WEEK
-# premise trend
+
 
