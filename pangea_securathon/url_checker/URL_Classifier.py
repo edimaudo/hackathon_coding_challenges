ece4644872c89cd70c508acef776fac74126e678
Edima <edimaudo@gmail.com>
2023-04-22 14:47:08 -0400

    Update URL_Classifier.py

--- pangea_securathon/url_checker/URL_Classifier.py
+++ pangea_securathon/url_checker/URL_Classifier.py
@@ -2,10 +2,14 @@ from utils import *
 
 st.title(APP_NAME)
 
-def check_api(URL):
-    pass
+def is_string_an_url(url_string):
+    url_string.strip()
+    result = validators.url(url_string)
+    if isinstance(result, ValidationFailure):
+        return False
+    return result
 
-def check_url(URL):
+def check_api(URL):
     pass
 
 def check_whois(URL):
@@ -13,13 +17,16 @@ def check_whois(URL):
 
 def url_option():
     url_text = st.text_input(URL_TEXT, '') 
-
     url_option_button = st.button('Check URL')
     if url_option_button:
         # check if a URL is entered
-        if len(url_text) < 1:
-            st.error('Please enter a URL', icon="🚨")
+        #if len(url_text) < 1:
+        #    st.error('Please enter a URL', icon="🚨")
         # check for valid URL
+        if is_string_an_url(url_text):
+            check_api(url_text)
+        else:
+            st.error('Please enter a valid URL', icon="🚨")
 
 
 def main():
