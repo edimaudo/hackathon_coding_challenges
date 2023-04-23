7ae531ed07bd7c3aed952c4729fa939c889cfd38
Edima <edimaudo@gmail.com>
2023-04-23 13:12:46 -0400

    Update utils.py

--- pangea_securathon/url_checker/utils.py
+++ pangea_securathon/url_checker/utils.py
@@ -9,11 +9,14 @@ import random
 import whois
 import validators
 from validators import ValidationFailure
+import pangea.exceptions as pe
+from pangea.config import PangeaConfig
+from pangea.services import Audit
+import requests
+import json
 
 # Text
 APP_NAME = 'URL Classifier'
-URL_TEXT = 'Enter a URL, press enter and then click the Check URL button'
-
 
 st.set_page_config( 
     page_title=APP_NAME,
