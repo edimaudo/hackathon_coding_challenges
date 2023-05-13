05fb71cf2deb5d83f3d012e097400a2858410067
Edima <edimaudo@gmail.com>
2023-05-13 12:37:38 -0400

    Create 2_About.py

---
+++ pangea_securathon2/mesh_intelligence/pages/2_About.py
@@ -0,0 +1,7 @@
+from utils import * 
+st.title(APP_NAME)
+
+st.markdown("""
+Web tool that provides insights for IP, URL and Domain information.
+The app uses Pangea url, IP, domain and embargo APIs.
+""")
