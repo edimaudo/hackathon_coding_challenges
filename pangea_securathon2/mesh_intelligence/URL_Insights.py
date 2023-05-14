4ea167d350944e736c00a6b47d882d5ce68797a5
Edima <edimaudo@gmail.com>
2023-05-14 19:16:29 -0400

    Update URL_Insights.py

--- pangea_securathon2/mesh_intelligence/URL_Insights.py
+++ pangea_securathon2/mesh_intelligence/URL_Insights.py
@@ -11,45 +11,47 @@ def is_string_a_url(url_string):
         return False
     return result
 
-def is_string_a_domain(domain_string):
-    domain_string.strip()
-    result = validators.domain(domain_string)
-    if isinstance(result, ValidationFailure):
-        return False
-    return result
-
-@st.cache_resource
-def generate_api_result(link, link_type):
-    headers = {'Authorization': AUTHORIZATION,'Content-Type': 'application/json',}
-    if link_type == 'URL':
-        json_data = {'provider': 'crowdstrike','url': link,}
-        response = requests.post(URL_LINK, headers=headers,json=json_data,)
-    else:
-        json_data = {'provider': 'domaintools','domain': link,}
-        response = requests.post(DOMAIN_LINK, headers=headers,json=json_data,)
-    return response
-
 def generate_whois_result(URL):
     w = whois.whois(URL)
     return w
 
 def url_analysis():
-    pass
+    url_text = st.text_input('Enter a URL , press enter and then click the Check URL button', '', 
+    placeholder = 'Enter a url like this -> http://www.example.com') 
+    url_button = st.button('Check URL')
 
-def domain_analysis():
-    pass
-    
+    if url_button:
+        if is_string_a_url(url_text):
+            url_output = generate_api_result(url_text,'URL')
+            url_output_data = url_output.json()
+            whois_output = generate_whois_result(url_text)
+            with st.container():
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.metric("Status",str(url_output_data['status']))
+                with col2:
+                    st.metric("URL Malicious Intent",str(url_output_data['result']['data']['verdict']))
+            st.write(" ")
+            col3, col4 = st.columns(2)
+            with col3:
+                st.metric("Creation Date",str(whois_output['creation_date']) )
+                st.metric("Expiration Date",str(whois_output['expiration_date']))
+            with col4:
+                st.metric("Domain Name",str(whois_output['domain_name']))
+                st.write("Server Names")
+                if whois_output['name_servers'] is None:
+                    st.write("None")
+                elif len(whois_output['name_servers']) < 2:
+                    st.write(str(server))
+                else:
+                    for server in whois_output['name_servers']:
+                        st.write("- ", str(server))
+        else:
+            st.error('Please enter a valid URL', icon="🚨")
 
 
 def main():
-    with st.sidebar:
-        choice = st.radio("Select an option",('URL', 'Domain'))
-        if choice == 'URL':
-            pass
-        else:
-            pass
-
-    
+    url_analysis()
 
 if __name__ == main():
     main()
