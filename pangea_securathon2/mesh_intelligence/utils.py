b60206a801b312d6bda481190d6d3a9f1d668a3c
Edima <edimaudo@gmail.com>
2023-05-15 18:51:08 -0400

    Update utils.py

--- pangea_securathon2/mesh_intelligence/utils.py
+++ pangea_securathon2/mesh_intelligence/utils.py
@@ -27,7 +27,10 @@ def generate_api_result(link, link_type):
     if link_type == 'URL':
         json_data = {'provider': 'crowdstrike','url': link,}
         response = requests.post(URL_LINK, headers=headers,json=json_data,)
-    else:
+    elif link_type == 'Domain':
         json_data = {'provider': 'domaintools','domain': link,}
         response = requests.post(DOMAIN_LINK, headers=headers,json=json_data,)
+    elif link_type == 'IP':
+        json_data = {'provider': 'crowdstrike','ip': link,}
+        response = requests.post(IP_LINK, headers=headers,json=json_data,)
     return response
