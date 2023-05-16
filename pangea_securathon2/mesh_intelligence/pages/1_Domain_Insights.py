47e63797a40b481d31282ad20bd2a82931703b64
Edima <edimaudo@gmail.com>
2023-05-15 20:49:43 -0400

    Update 1_Domain_Insights.py

--- pangea_securathon2/mesh_intelligence/pages/1_Domain_Insights.py
+++ pangea_securathon2/mesh_intelligence/pages/1_Domain_Insights.py
@@ -11,9 +11,24 @@ def is_string_a_domain(domain_string):
         return False
     return result
 
-
 def domain_analysis():
-    pass
+    domain_text = st.text_input('Enter a Domain , press enter and then click the Check Domain button', '', 
+    placeholder = 'Enter a domain like this -> example.com') 
+    domain_button = st.button('Check Domain')
+
+    if domain_button:
+        if is_string_a_domain(domain_text):
+            domain_output = generate_api_result(domain_text,'Domain')
+            domain_output_data = domain_output.json()
+            with st.container():
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.metric("Status",str(domain_output_data['status']))
+                with col2:
+                    st.metric("URL Malicious Intent",str(domain_output_data['result']['data']['verdict']))
+            st.write(" ")
+        else:
+            st.error('Please enter a valid Domain', icon="🚨")
     
 
 
