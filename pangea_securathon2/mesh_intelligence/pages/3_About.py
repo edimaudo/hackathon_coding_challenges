bc04cfe94c366fe3c43a0fefbea19a6869c27c5c
Edima <edimaudo@gmail.com>
2023-05-15 20:52:02 -0400

    Update 3_About.py

--- pangea_securathon2/mesh_intelligence/pages/3_About.py
+++ pangea_securathon2/mesh_intelligence/pages/3_About.py
@@ -2,6 +2,5 @@ from utils import *
 st.title(APP_NAME)
 
 st.markdown("""
-Web tool that provides insights for IP, URL and Domain information.
-The app uses Pangea url, IP, domain and embargo APIs.
+Web tool that provides insights for IP, URL and Domain information using different Pangea APIs.
 """)
