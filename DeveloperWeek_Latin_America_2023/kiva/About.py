69076fe07359e77b35beb4057609b5a0d9be3916
Edima <edimaudo@gmail.com>
2023-06-05 16:55:42 -0400

    Create About.py

---
+++ DeveloperWeek_Latin_America_2023/kiva/About.py
@@ -0,0 +1,15 @@
+from utils import * 
+
+st.title(APP_NAME)
+st.header(APP_ABOUT)
+st.markdown("""
+### Kiva
+[Kiva](https://www.kiva.org) is a social impact organization with a mission to expand financial access to help under-served individuals and communities thrive.  Kiva's goal is to improve poverty alleviation and financial inclusion.  
+
+### The Application
+The application leverages Kiva's data snapshot to build a web app that focuses on three key areas:
+- **Loan Insights**: Provides loan Insights by country, sector and activities.
+- **Risk Management**: Applies Portfolio Theory to the loans to provide operational support to balance loan risk.
+- **Loan impact**: Leverages the SROI framework to show the impact of the loans.
+ 
+""")
