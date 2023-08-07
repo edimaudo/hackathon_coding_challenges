7fe0a22376e1252512739143f0e185d33d710a33
Edima <edimaudo@gmail.com>
2023-08-07 15:43:41 -0400

    pangea
    deleted pangea code for securathon

--- pangea_securathon2/mesh_intelligence/config.py
+++
@@ -1,8 +0,0 @@
-
-
-ACCESS_TOKEN = "pts_43rv7yxmu7hneyohnwz5prnqhi4zwi5k"
-CONFIG_ID = 'pcf_3kxwr4dbpbzs6o47hejb2r5a4dmkqklv'
-DOMAIN = 'aws.us.pangea.cloud'
-AUTHORIZATION = "Bearer " +  ACCESS_TOKEN
-
-
--- pangea_securathon2/mesh_intelligence/pages/3_About.py
+++
@@ -1,6 +0,0 @@
-from utils import * 
-st.title(APP_NAME)
-
-st.markdown("""
-Web tool that provides insights for IP, URL and Domain information using different Pangea APIs.
-""")
