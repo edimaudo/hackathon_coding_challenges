333b25921905103afb9b1b24c9154a5624545247
Edima <edimaudo@gmail.com>
2023-08-28 17:43:13 -0400

    app folder
    Added information into app folder

---
+++ pangea_securathon_2023/app/libraries.py
@@ -0,0 +1,13 @@
+"""
+Libraries
+"""
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import os, os.path
+import warnings
+import numpy as np
+import random
+from datetime import datetime
+import math 
+
---
+++ pangea_securathon_2023/app/utils.py
@@ -0,0 +1,14 @@
+"""
+Application Text
+"""
+APP_NAME = 'Varainhoito CRM'
+APP_HOME = 'HOME'
+APP_CLIENT = 'CLIENT'
+
+
+"""
+Application Title
+"""
+st.set_page_config( 
+    page_title=APP_NAME,
+)
