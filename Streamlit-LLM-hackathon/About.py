78a1322016c75aea8c8897da568532f0a34d5c16
Edima <edimaudo@gmail.com>
2023-09-09 10:13:33 -0400

    Update About.py

--- Streamlit-LLM-hackathon/About.py
+++ Streamlit-LLM-hackathon/About.py
@@ -2,3 +2,11 @@ from utils import *
 
 st.title(APP_NAME)
 st.header(APP_OVERVIEW)
+
+st.write("""
+Printers! We all have a love-hate relationship with them.  When things are going well it is perfect. Just one glitch or driver issue and all hell breaks lose.
+There has been a proliferation of printer companion apps to make the printing process easier.  
+These apps are what someone might use to print remotely or scan on the go.
+The goal is perform text analysis on companion app reviews.  The data was scraped from the Google Play store. 
+The apps analyzed are Epson SmartPanel, Epson iPrint, HP Smart and Canon Print.
+""")
