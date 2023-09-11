92897bc68314bdf90e35598431e499485b4d9e82
Edima <edimaudo@gmail.com>
2023-09-11 14:50:49 -0400

    Update 2_Data_Insights.py

--- Streamlit-LLM-hackathon/Pages/2_Data_Insights.py
+++ Streamlit-LLM-hackathon/Pages/2_Data_Insights.py
@@ -1,4 +1,3 @@
-
 from utils import * 
 
 st.title(APP_NAME)
@@ -9,8 +8,8 @@ app_list.sort()
 app_choice = st.multiselect("Companion App",app_list,app_list)
 analysis = df_analysis[df_analysis['app'].isin(app_choice)]
     
-# Average Printer Score
-st.subheader("App Score")
+Printer Score
+st.subheader("Companion App Score")
 printer_score = analysis[['app','score']]
 printer_score_agg = printer_score.groupby('app').agg(Total = ('score', 'mean')).reset_index()
 printer_score_agg.columns = ['Companion App', 'Score']
@@ -19,7 +18,7 @@ fig = px.bar(printer_score_agg, x="Score", y="Companion App", orientation='h')
 st.plotly_chart(fig)
 
 # Printer review count
-st.subheader("App review Count")
+st.subheader("Companion App review Count")
 printer_count = analysis[['app', 'reviewId']]
 printer_count_agg = printer_count.groupby(['app'])['reviewId'].agg('count').reset_index()
 printer_count_agg.columns = ['Companion App', '# of Reviews']
@@ -28,7 +27,7 @@ fig = px.bar(printer_count_agg, x="# of Reviews", y="Companion App", orientation
 st.plotly_chart(fig)
 
 # Printer Score over time
-st.subheader("App Score trend")
+st.subheader("Companion App Score trend")
 printer_score_time = analysis[['app','score','Date']]
 printer_score_time_agg = printer_score_time.groupby(['app','Date']).agg(Total = ('score', 'mean')).reset_index()
 printer_score_time_agg.columns = ['Companion App', 'Date','Score']
@@ -37,7 +36,7 @@ fig = px.line(printer_score_time_agg, x="Date", y="Score",color='Companion App')
 st.plotly_chart(fig)
     
 # Average thumbs up by selected printer
-st.subheader("AppThumbs Up Count")
+st.subheader("Companion App Thumbs Up Count")
 printer_thumbsup = analysis[['app','thumbsUpCount']]
 printer_thumbsup_agg = printer_thumbsup.groupby('app').agg(Total = ('thumbsUpCount', 'mean')).reset_index()
 printer_thumbsup_agg.columns = ['Companion App', 'Thumbs Up']
