7a5b5afec74ff9fe97520ee80c3a8e4e7917fff1
Edima <edimaudo@gmail.com>
2023-09-17 07:27:56 -0400

    Update About.py

--- Youth_Data_Hackathon/About.py
+++ Youth_Data_Hackathon/About.py
@@ -3,5 +3,5 @@ from utils import *
 st.title(APP_NAME)
 st.header(ABOUT_HEADER)
 st.write("""
-
+A web app that analyzes the wellbeing metrics and crime data in different neighbohoods in Toronto, ON, Canada.
 """)
