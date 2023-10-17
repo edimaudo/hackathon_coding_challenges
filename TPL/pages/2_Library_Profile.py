54d6069615363d525da77d7b4e7f4cefc2b6cec3
Edima <edimaudo@gmail.com>
2023-10-17 17:06:22 -0400

    Update 2_Library_Profile.py

--- TPL/pages/2_Library_Profile.py
+++ TPL/pages/2_Library_Profile.py
@@ -13,6 +13,7 @@ top_container = st.container()
 middle_container = st.container()
 bottom_container = st.container()
 
+
 with top_container:
     st.subheader("Branch Profile")
     col1, col2 = st.columns(2)
@@ -31,32 +32,61 @@ with top_container:
 
 with middle_container:
     st.subheader("Branch Features")
-    col3, col4 = st.columns(2)
-    with col3:
+    col1, col2 = st.columns(2)
+    with col1:
         st.write("Square Footage")
         st.write("No of workstations")
         st.write("Public Parking Available")
         st.write("Service Tier")
-    with col4:
+    with col2:
         st.write("Adult literacy Program Avilable")
         st.write("Computer learning centre Avilable")
         st.write("Digital Innovation Hub Avilable")
         st.write("Youth Hub Avilable")
 
 
-    
 
+with bottom_container:
+    st.subheader("Branch Trends")
+    st.markdown("**Annual Vists**")
+    df = visits_branch[['Year','BranchCode','Visits']]
+    df = df[(df['BranchCode'] == branch_df['BranchCode'][0])]
+    df = df[['Year','Visits']]
+    df= df.groupby(['Year']).agg(Total_visits = ('Visits', 'sum')).reset_index()
+    df.columns = ['Year','Total Visits']
+    df = df.sort_values("Year", ascending=True).reset_index()
+    fig = px.bar(df, x="Year", y="Total Visits")
+    st.plotly_chart(fig)
 
+    st.markdown("**Annual Registrations**")
+    df = card_registration_branch[['Year','BranchCode','Registrations']]
+    df = df[(df['BranchCode'] == branch_df['BranchCode'][0])]
+    df = df[['Year','Registrations']]
+    df= df.groupby(['Year']).agg(Total_registrations = ('Registrations', 'sum')).reset_index()
+    df.columns = ['Year','Total Registrations']
+    df = df.sort_values("Year", ascending=True).reset_index()
+    fig = px.bar(df, x="Year", y="Total Registrations")
+    st.plotly_chart(fig)
 
+    st.markdown("**Annual Circulations**")
+    df = circulation_branch[['Year','BranchCode','Circulation']]
+    df = df[(df['BranchCode'] == branch_df['BranchCode'][0])]
+    df = df[['Year','Circulation']]
+    df= df.groupby(['Year']).agg(Total_circulations = ('Circulation', 'sum')).reset_index()
+    df.columns = ['Year','Total Circulations']
+    df = df.sort_values("Year", ascending=True).reset_index()
+    fig = px.bar(df, x="Year", y="Total Circulations")
+    st.plotly_chart(fig)
 
-
-st.subheader("Branch Trends")
-# annual visits
-# registrations
-# Annual Circulation
-# Annual Workstation Usage
-
-# Wellheing index
+    st.markdown("**Annual Workstation Usage**")
+    df = workstation_usage_branch[['Year','BranchCode','Sessions']]
+    df = df[(df['BranchCode'] == branch_df['BranchCode'][0])]
+    df = df[['Year','Sessions']]
+    df= df.groupby(['Year']).agg(Total_sessions = ('Sessions', 'sum')).reset_index()
+    df.columns = ['Year','Total Sessions']
+    df = df.sort_values("Year", ascending=True).reset_index()
+    fig = px.bar(df, x="Year", y="Total Sessions")
+    st.plotly_chart(fig)
 
 
 
