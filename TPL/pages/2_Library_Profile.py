876a0a017d6edb85f06f056c44c4bab2eec67485
Edima <edimaudo@gmail.com>
2023-10-17 15:43:16 -0400

    Update 2_Library_Profile.py

--- TPL/pages/2_Library_Profile.py
+++ TPL/pages/2_Library_Profile.py
@@ -4,27 +4,58 @@ st.image(img)
 st.title(APP_NAME)
 st.header(LIBRARY_PROFILE_HEADER)
 
-#branch Insights
+#side bar library name dropdown
+branch_option = st.sidebar.selectbox('Branches',branches)
+
+
+st.subheader("Branch Profile")
+branch_df =  general_info_branch[(general_info_branch.BranchName == branch_option)]   #general_info_branch[(general_info_branch['BranchName']==(branch_option))]
+top_metric_container = st.container()
+middle_metric_container = st.container()
+bottom_metric_container = st.container()
+col1, col2,col3 = st.columns(3)
+with st.container():
+    with col1:
+        st.metric("Branch Name",branch_df['BranchName'])
+        st.metric("Address",branch_df['Address'])
+        st.metric("Present Site Year",branch_df['PresentSiteYear'])
+    with col2:
+        st.metric("Branch Name",branch_df['BranchName'])
+        st.metric("Address",branch_df['Address'])
+        st.metric("Present Site Year",branch_df['PresentSiteYear'])
+    with col3:
+        st.metric("Branch Name",branch_df['BranchName'])
+        st.metric("Address",branch_df['Address'])
+        st.metric("Present Site Year",branch_df['PresentSiteYear'])
 
 #branch profile
-#collection size
-# of visits trend
-# of cardholders trend
-# collection trend
-#new registration trend
-#program type trend
-#program by age group
+#BranchName BranchCode # PhysicalBranch
+#Address	PostalCode	WardName
+#PresentSiteYear #Website	Telephone		
 
-##branch unique features
+#branch location
+#Add geojson data + #Lat	Long
+
+st.subheader("Branch Features")
+##branch features
+#SquareFootage	PublicParking
+# Workstations
+#ServiceTier
+#AdultLiteracyProgram	
 #computer learning centre available
 #digital innovation hub 
-#kid stop available
 #kid stop early learning centre
 #Youth advisory
 #youth hub
-#workstation users
-#workstations
-#branch location
 
-#wellbeing index#
-#Add LLM using Langchain —> https://python.langchain.com/docs/modules/agents/toolkits/pandas.html
+st.subheader("Branch Trends")
+# annual visits
+# registrations
+# Annual Circulation
+# Annual Workstation Usage
+
+# Wellheing index
+
+
+
+
