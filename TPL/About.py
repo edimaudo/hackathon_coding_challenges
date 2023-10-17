d2486bdd8cc00c6f6bc932c0d3fc03391ffb3474
Edima <edimaudo@gmail.com>
2023-10-17 13:52:49 -0400

    Update About.py

--- TPL/About.py
+++ TPL/About.py
@@ -4,7 +4,7 @@ from utils import *
 
 st.image(img)
 st.title(APP_NAME)
-st.header(OVERVIEW_HEADER)
+st.header(ABOUT_HEADER)
 st.write("""
 The goal is to leverage open data from Toronto Public Library data to understand how denizens are using library services.  The data is from 
 Data is from [TPL Open data catalogue](https://opendata.tpl.ca).
