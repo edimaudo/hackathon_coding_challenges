9698556cae2ca197540d52d15379b65731800a3f
Edima <edimaudo@gmail.com>
2023-10-17 13:52:53 -0400

    Update 2_Library_Profile.py

---
+++ TPL/pages/2_Library_Profile.py
@@ -0,0 +1,29 @@
+from utils import *
+
+st.image(img)
+st.title(APP_NAME)
+st.header(LIBRARY_PROFILE_HEADER)
+
+#branch Insights
+
+#branch profile
+#collection size
+# of visits trend
+# of cardholders trend
+# collection trend
+#new registration trend
+#program type trend
+#program by age group
+
+##branch unique features
+#computer learning centre available
+#digital innovation hub 
+#kid stop available
+#kid stop early learning centre
+#Youth advisory
+#youth hub
+#workstation users
+#workstations
+#branch location
+
+#wellbeing index#
