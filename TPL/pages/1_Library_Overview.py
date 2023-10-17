1dc552b73737b7754eec46961b8474d48c37c6c2
Edima <edimaudo@gmail.com>
2023-10-17 11:52:21 -0400

    Update 1_Library_Overview.py

--- TPL/pages/1_Library_Overview.py
+++ TPL/pages/1_Library_Overview.py
@@ -6,3 +6,20 @@ st.image(img)
 st.title(APP_NAME)
 st.header(LIBRARY_OVERVIEW_HEADER)
 
+#overview
+#ability to see all branches
+# of visits
+# of branches
+# of cardholders
+#collection zie
+# collection trend
+# registration trends
+# of programs
+# of workstation trend
+#program type trend
+#program by age group
+#workstation users
+##workstations
+#workstation usage
+
+#Add LLM using Langchain —> https://python.langchain.com/docs/modules/agents/toolkits/pandas.html
