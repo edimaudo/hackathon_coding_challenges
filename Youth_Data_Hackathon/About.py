95a36fadbe9c4649b09ff2d7055edbb39f4a0e7a
Edima <edimaudo@gmail.com>
2023-10-17 11:34:27 -0400

    Delete About.py

--- Youth_Data_Hackathon/About.py
+++
@@ -1,9 +0,0 @@
-from utils import * 
-
-st.title(APP_NAME)
-st.header(ABOUT_HEADER)
-st.write("""
-A web app that analyzes the wellbeing metrics and crime data in different neighbohoods in Toronto, ON, Canada.
-Data is from [Toronto Police Service](https://data.torontopolice.on.ca/pages/major-crime-indicators) and 
-[Toronto Open Data](https://www.toronto.ca/city-government/data-research-maps/open-data/)
-""")
