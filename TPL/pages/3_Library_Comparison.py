ff5ff0c7a7a951be804a679d8cf73b5e119ed5c1
Edima <edimaudo@gmail.com>
2023-10-17 15:43:13 -0400

    Update 3_Library_Comparison.py

--- TPL/pages/3_Library_Comparison.py
+++ TPL/pages/3_Library_Comparison.py
@@ -9,27 +9,30 @@ st.header(LIBRARY_COMPARISON_HEADER)
 #branch comparison
 
 ##branch profile
-
-#Unique features
-
-#wellbeing index
-
 #branch profile
-#collection size
-# of visits trend
-# of cardholders trend
-# collection trend
-#new registration trend
-#program type trend
-#program by age group
+#BranchName BranchCode # PhysicalBranch
+#Address	PostalCode	PresentSiteYear
+#WardNo	WardName
+#Website	Telephone		
+#Lat	Long
+#branch location - #Add geojson data
 
-##branch unique features
+##branch features
+#SquareFootage	PublicParking
+# Workstations
+#ServiceTier
+#AdultLiteracyProgram	
 #computer learning centre available
 #digital innovation hub 
-#kid stop available
 #kid stop early learning centre
 #Youth advisory
 #youth hub
-#workstation users
-#workstations
-#branch location
+
+#Trends
+# annual visits
+# registrations
+# Annual Circulation
+# Annual Workstation Usage
+
+#wellbeing index#
+#Add LLM using Langchain —> https://python.langchain.com/docs/modules/agents/toolkits/pandas.html
