45f3528e09d2a38fce1efc51efae14533d1ddd23
Edima <edimaudo@gmail.com>
2023-10-17 13:52:55 -0400

    Update 3_Library_Comparison.py

---
+++ TPL/pages/3_Library_Comparison.py
@@ -0,0 +1,35 @@
+from utils import *
+
+st.image(img)
+st.title(APP_NAME)
+st.header(LIBRARY_COMPARISON_HEADER)
+
+
+
+#branch comparison
+
+##branch profile
+
+#Unique features
+
+#wellbeing index
+
+#branch profile
+#collection size
+# of visits trend
+# of cardholders trend
+# collection trend
+#new registration trend
+#program type trend
+#program by age group
+
+##branch unique features
+#computer learning centre available
+#digital innovation hub 
+#kid stop available
+#kid stop early learning centre
+#Youth advisory
+#youth hub
+#workstation users
+#workstations
+#branch location
