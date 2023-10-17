ed38c952912c9f4ad375be0d09eca21904bbc387
Edima <edimaudo@gmail.com>
2023-10-17 13:52:57 -0400

    Update utils.py

--- TPL/utils.py
+++ TPL/utils.py
@@ -12,7 +12,7 @@ import random
 #from pycaret.classification import *
 
 """
-Dashboard Text
+Dashboard Information
 """
 APP_NAME = 'Toronto Public Library Insights'
 OVERVIEW_HEADER = 'Overview'
@@ -33,21 +33,31 @@ img = "img/tpl_image.png"
 
 # Load data
 @st.cache_data
-def load_data(filetype,data_file):
-    if filetype == "csv":
-        data = pd.read_csv(data_file)
-    else:
-        data = pd.read_excel(data_file)
+def load_data(data_file):
+    data = pd.read_csv(data_file)
     return data
 
 # Wellbeing data
-wellbeing_economics = load_data("csv","data/wellbeing-toronto-economics.csv")
-wellbeing_culture = load_data("csv","data/wellbeing-toronto-culture.csv")
-wellbeing_health = load_data("csv","data/wellbeing-toronto-health.csv")
-wellbeing_transportation = load_data("csv","data/wellbeing-toronto-transportation.csv")
-wellbeing_education = load_data("csv","data/wellbeing-toronto-education.csv")
+wellbeing_economics = load_data("data/wellbeing-toronto-economics.csv")
+wellbeing_culture = load_data("data/wellbeing-toronto-culture.csv")
+wellbeing_health = load_data("data/wellbeing-toronto-health.csv")
+wellbeing_transportation = load_data("data/wellbeing-toronto-transportation.csv")
+wellbeing_education = load_data("data/wellbeing-toronto-education.csv")
 
-#library data
-
+# library data
+#Neighbourhoods.geojson
+computer_learning_centre = load_data("data/Computer_Learning_Centres.csv")
+digital_innovation_hub = load_data("data/Digital_Innovation_Hubs.csv")
+kid_stop = load_data("data/KidsStop_Early_Literacy_Centres.csv")
+neighorhood_improvement = load_data("data/Neighbourhood_Improvement_Area_Branches.csv")
+general_info_branch = load_data("data/tpl-branch-general-information-2023.csv")
+card_registration_branch = load_data("data/tpl-card-registrations-annual-by-branch-2012-2022.csv")
+circulation_branch = load_data("data/tpl-circulation-annual-by-branch-2012-2022.csv")
+visits_branch = load_data("data/tpl-visits-annual-by-branch-2012-2022.csv")
+workstation_usage_branch = load_data("data/tpl-workstation-usage-annual-by-branch-2012-2022.csv")
+workstation_users = load_data("data/Workstation_Users.csv")
+workstations = load_data("data/Workstations.csv")
+youth_advisory = load_data("data/Youth_Advisory_Groups_Locations.csv")
+youth_hubs = load_data("data/Youth_Hubs_Locations.csv")
 
 # Data munging
