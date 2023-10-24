ea53127850b4959acd0a747f9158b34a4aec0241
Edima <edimaudo@gmail.com>
2023-10-23 20:13:31 -0400

    Update 4_About.py

--- TPS/pages/4_About.py
+++ TPS/pages/4_About.py
@@ -1,12 +1,10 @@
 # Libraries
-import streamlit as st
+from utils import * 
+st.title(APP_NAME)
 
-st.title("Toronto Crime Analysis App")
-st.markdown('This app provides insights into crime trends occurring in Toronto, Canada.  Data is from [Toronto Police Service](https://data.torontopolice.on.ca/pages/major-crime-indicators) and [Toronto Open Data](https://www.toronto.ca/city-government/data-research-maps/open-data/)')
-st.markdown("""
-The application has 4 sections:
-- **Crime Overview**
-- **Crime Type Exploration**
-- **Neighbourhood Exploration**
-- **Crime Predictor** 
-""")
+st.markdown(
+    """
+    This app provides insights into crime trends and wellbeing in Toronto neigborhoods.  
+    Data is from [Toronto Police Service](https://data.torontopolice.on.ca/pages/major-crime-indicators) 
+    and [Toronto Open Data](https://www.toronto.ca/city-government/data-research-maps/open-data/).
+    """)
