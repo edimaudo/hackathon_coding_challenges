e26cde324cb6b7c1203724b064b9090a03cc5ef4
Edima <edimaudo@gmail.com>
2023-12-14 11:06:44 -0500

    Create About.py

---
+++ Spotify/About.py
@@ -0,0 +1,16 @@
+from utils import * 
+
+st.title(APP_NAME)
+st.header(APP_ABOUT)
+st.markdown("""
+### Spotify
+[Spotify](https://open.spotify.com/) is the world's most popular audio streaming subscription service with more than 574 million users, including 226 million subscribers in more than 180 countries
+
+### About The Application
+The application leverages Spotify data snapshot that focuses on the areas listed below
+- **Demographic Insights**
+- **Music**
+- **Podcasts**
+- **Subscription Prediction**
+
+""")
