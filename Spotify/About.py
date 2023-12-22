c69e1485aa3a22106b9278b54d4feea1f63c479e
Edima <edimaudo@gmail.com>
2023-12-22 17:45:11 -0500

    Update About.py

--- Spotify/About.py
+++ Spotify/About.py
@@ -7,7 +7,6 @@ st.markdown(
 [Spotify](https://open.spotify.com/) is the world's most popular audio streaming subscription service with more than 574 million users, including 226 million subscribers in more than 180 countries
 
 The application leverages Spotify data snapshot that focuses on the areas listed below
-- **Demographic Insights**
 - **Music**
 - **Podcasts**
 - **Subscription Prediction**
