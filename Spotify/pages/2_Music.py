57c92c818cf0c1a618c9c93ec2bc4413b427d2fc
Edima <edimaudo@gmail.com>
2023-12-22 19:30:28 -0500

    Update 2_Music.py

--- Spotify/pages/2_Music.py
+++ Spotify/pages/2_Music.py
@@ -3,4 +3,11 @@ from utils import *
 st.title(APP_NAME)
 st.header(APP_MUSIC)
 
+with st.sidebar:
+    age_option = st.multiselect("Age",age,age)
+    gender_option = st.multiselect("Gender",gender,gender)
+    usage_option = st.multiselect("Usage",usage,usage)
+    subscription_option = st.multiselect("Subscription",subscription,subscription)
 
+
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, = st.tabs(["Cat", "Dog", "Owl","Cat", "Dog", "Owl","Cat", "Dog"])
