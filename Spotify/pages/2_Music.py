5cf74252cc37836d695e53d4d5a49a66d671e3d3
Edima <edimaudo@gmail.com>
2023-12-24 08:49:14 -0500

    Update 2_Music.py

--- Spotify/pages/2_Music.py
+++ Spotify/pages/2_Music.py
@@ -10,23 +10,28 @@ with st.sidebar:
     subscription_option = st.multiselect("Subscription",subscription,subscription)
 
 with st.container():
-    container = st.container()
-    #middle_container = st.container()
-    #bottom_container = st.container()
-    col1, col2 = st.columns(2)
     music_df = df[(df['Age'].isin(age_option)) & (df['Gender'].isin(gender_option)) & (df['spotify_usage_period'].isin(usage_option)) & (df['spotify_subscription_plan'].isin(subscription_option))]
-    with container:
-        with col1:
-            pass
-        with col2:
-            pass
-    with container:
-        with col1:
-            pass
-        with col2:
-            pass
-    with container:
-        with col1:
-            pass
-        with col2:
-            pass
+    
+    st.subheader("Listening Devices")
+    output_df = df[['spotify_listening_device']]
+    output_df = output_df.groupby(['spotify_listening_device']).agg(Count = ('spotify_listening_device', 'count')).reset_index()
+    output_df.columns = ['Listening Device', 'Count']
+    output_df.sort_values("Count", ascending=True, inplace=True)
+    fig = px.bar(output_df, x="Count", y="Listening Device",orientation='h')
+    st.plotly_chart(fig)
+           
+    st.subheader("Genres")
+    output_df = df[['fav_music_genre']]
+    output_df = output_df.groupby(['fav_music_genre']).agg(Count = ('fav_music_genre', 'count')).reset_index()
+    output_df.columns = ['Music Genre', 'Count']
+    output_df.sort_values("Count", ascending=True,inplace=True)
+    fig = px.bar(output_df, x="Count", y="Music Genre",orientation='h')
+    st.plotly_chart(fig)
+
+    st.subheader('Music Time Slot')
+    output_df = df[['music_time_slot']]
+    output_df = output_df.groupby(['music_time_slot']).agg(Count = ('music_time_slot', 'count')).reset_index()
+    output_df.columns = ['Music Time slot', 'Count']
+    output_df.sort_values("Count", ascending=False, inplace=True)
+    fig = px.pie(output_df, values='Count', names='Music Time slot')
+
