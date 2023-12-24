4d13e6708a3c1cca72bd4fb0c8bf4e8a2ab173c2
Edima <edimaudo@gmail.com>
2023-12-24 13:39:50 -0500

    Update 2_Music.py

--- Spotify/pages/2_Music.py
+++ Spotify/pages/2_Music.py
@@ -12,8 +12,8 @@ with st.sidebar:
 with st.container():
     music_df = df[(df['Age'].isin(age_option)) & (df['Gender'].isin(gender_option)) & (df['spotify_usage_period'].isin(usage_option)) & (df['spotify_subscription_plan'].isin(subscription_option))]
     
-    st.subheader("Listening Devices")
-    output_df = df[['spotify_listening_device']]
+    st.subheader("Listening Style")
+    output_df = music_df[['spotify_listening_device']]
     output_df = output_df.groupby(['spotify_listening_device']).agg(Count = ('spotify_listening_device', 'count')).reset_index()
     output_df.columns = ['Listening Device', 'Count']
     output_df.sort_values("Count", ascending=True, inplace=True)
@@ -21,7 +21,7 @@ with st.container():
     st.plotly_chart(fig)
            
     st.subheader("Genres")
-    output_df = df[['fav_music_genre']]
+    output_df = music_df[['fav_music_genre']]
     output_df = output_df.groupby(['fav_music_genre']).agg(Count = ('fav_music_genre', 'count')).reset_index()
     output_df.columns = ['Music Genre', 'Count']
     output_df.sort_values("Count", ascending=True,inplace=True)
@@ -29,9 +29,10 @@ with st.container():
     st.plotly_chart(fig)
 
     st.subheader('Music Time Slot')
-    output_df = df[['music_time_slot']]
+    output_df = music_df[['music_time_slot']]
     output_df = output_df.groupby(['music_time_slot']).agg(Count = ('music_time_slot', 'count')).reset_index()
     output_df.columns = ['Music Time slot', 'Count']
     output_df.sort_values("Count", ascending=False, inplace=True)
     fig = px.pie(output_df, values='Count', names='Music Time slot')
+    st.plotly_chart(fig)
 
