08a742a6dd1320ba2292068879cfe0c9d95b3e80
Edima <edimaudo@gmail.com>
2023-12-24 08:14:43 -0500

    Update 2_Music.py

--- Spotify/pages/2_Music.py
+++ Spotify/pages/2_Music.py
@@ -9,5 +9,24 @@ with st.sidebar:
     usage_option = st.multiselect("Usage",usage,usage)
     subscription_option = st.multiselect("Subscription",subscription,subscription)
 
-
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, = st.tabs(["Cat", "Dog", "Owl","Cat", "Dog", "Owl","Cat", "Dog"])
+with st.container():
+    container = st.container()
+    #middle_container = st.container()
+    #bottom_container = st.container()
+    col1, col2 = st.columns(2)
+    music_df = df[(df['Age'].isin(age_option)) & (df['Gender'].isin(gender_option)) & (df['spotify_usage_period'].isin(usage_option)) & (df['spotify_subscription_plan'].isin(subscription_option))]
+    with container:
+        with col1:
+            pass
+        with col2:
+            pass
+    with container:
+        with col1:
+            pass
+        with col2:
+            pass
+    with container:
+        with col1:
+            pass
+        with col2:
+            pass
