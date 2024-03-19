4ef3606392df00600b8b0d4e43a8c48d56cf7f14
Edima <edimaudo@gmail.com>
2024-03-19 18:31:25 -0400

    Update app.R
    added libraries and updated skeleton

--- 2024_VSFG_Impact/VFSG/app.R
+++ 2024_VSFG_Impact/VFSG/app.R
@@ -1,15 +1,14 @@
 #================================================================================
 # Shiny web app which provides insights visualization for social good projects
 #================================================================================
-
 rm(list = ls())
 ################
-#packages 
+# Packages 
 ################
 packages <- c(
-  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT',
+  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT','readxl',
   'mlbench','caTools','gridExtra','doParallel','grid','forecast','reshape2',
-  'caret','dummies','tidyr','Matrix','lubridate','TTR',
+  'caret','dummies','tidyr','Matrix','lubridate','plotly',
   'data.table','scales','stopwords','tidytext',
   'stringr', 'textmineR','topicmodels','textclean'
 )
@@ -19,3 +18,27 @@ for (package in packages) {
     library(package, character.only=T)
   }
 }
+
+
+################
+# Load Data
+################
+charity <- read_excel("Charity - SDG.xlsx")
+
+################
+# Data Setup
+################
+
+
+################
+# UI
+################
+
+
+################
+# Server logic 
+################
+server <- function(input, output,session) {}
+
+shinyApp(ui, server)
+
