1e7956b0bd0653df7bd06643fd745ad2a8a34344
Edima <edimaudo@gmail.com>
2024-03-20 20:30:14 -0400

    Update app.R
    Added partner quote data

--- 2024_VSFG_Impact/VFSG/app.R
+++ 2024_VSFG_Impact/VFSG/app.R
@@ -25,12 +25,14 @@ for (package in packages) {
 ################
 charity_sdg <- read_excel("Charity - SDG.xlsx",sheet="SDG Goals")
 charity_impact <- read_excel("Charity - SDG.xlsx",sheet="Impact Data")
-linkedin <- read_excel("Linkedin Stats.xlsx")
+partner_quotes <- read_excel("partner_quotes.xlsx")
+linkedin <- read_excel("Linkedin Stats.xlsx",sheet="LI metrics")
+linkedin_posts <- read_excel("Linkedin Stats.xlsx",sheet="All posts")
 project_nepal <- read_excel("Build Up Nepal.xlsx")
 project_india <- read_excel("India Water Portal submissions.xlsx")
 project_sunny <- read_excel("Sunny Street Submissions.xlsx")
 project_tap <- read_excel("Tap Elderly Women_s Wisdom for Youth (TEWWY) Submissions.xlsx")
-#project_video <- read_excel("Video Volunteers Submissions.xlsx")
+project_video <- read_excel("Video Volunteers Submissions.xlsx")
 project_who <- read_excel("Who submissions.xlsx")
 
 
