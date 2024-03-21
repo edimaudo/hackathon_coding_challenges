bb617b2fceebd2524bd19e3cc8164f01fc98ae13
Edima <edimaudo@gmail.com>
2024-03-20 20:09:45 -0400

    Update app.R
    Added data information

--- 2024_VSFG_Impact/VFSG/app.R
+++ 2024_VSFG_Impact/VFSG/app.R
@@ -23,7 +23,16 @@ for (package in packages) {
 ################
 # Load Data
 ################
-charity <- read_excel("Charity - SDG.xlsx")
+charity_sdg <- read_excel("Charity - SDG.xlsx",sheet="SDG Goals")
+charity_impact <- read_excel("Charity - SDG.xlsx",sheet="Impact Data")
+linkedin <- read_excel("Linkedin Stats.xlsx")
+project_nepal <- read_excel("Build Up Nepal.xlsx")
+project_india <- read_excel("India Water Portal submissions.xlsx")
+project_sunny <- read_excel("Sunny Street Submissions.xlsx")
+project_tap <- read_excel("Tap Elderly Women_s Wisdom for Youth (TEWWY) Submissions.xlsx")
+#project_video <- read_excel("Video Volunteers Submissions.xlsx")
+project_who <- read_excel("Who submissions.xlsx")
+
 
 ################
 # Data Setup
