afe9fa1bb78f93c17c088e0f8fd2eda81075c70c
Edima <edimaudo@gmail.com>
2024-04-08 18:22:00 -0400

    Create app.R

---
+++ Apra/app/app.R
@@ -0,0 +1,21 @@
+#========================================
+# Shiny web app which provides insights 
+# for a fundraising project
+#=========================================
+rm(list = ls())
+#=============
+# Packages 
+#=============
+packages <- c(
+  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT','readxl',
+  'mlbench','caTools','gridExtra','doParallel','grid','forecast','reshape2',
+  'caret','dummies','tidyr','Matrix','lubridate','plotly','RColorBrewer',
+  'data.table','scales','stopwords','tidytext','stringr','wordcloud','wordcloud2',
+  'SnowballC','textmineR','topicmodels','textclean','tm'
+)
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
