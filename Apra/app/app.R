d7184e3a3d47ac1d95b3e8a079d8c5dba945b011
Edima <edimaudo@gmail.com>
2024-04-21 20:06:29 -0400

    Update app.R

--- Apra/app/app.R
+++ Apra/app/app.R
@@ -74,17 +74,17 @@ ui <- dashboardPage(
       tabItem(tabName = "segment",
               sidebarLayout(
                 sidebarPanel(width = 3,
-                  selectInput("campaignInput", "Campaign", 
-                              choices = campaign, selected = campaign, multiple = TRUE),
-                  selectInput("primaryUnitInput", "Primary Unit", 
-                              choices = primary_unit, selected = primary_unit,multiple = TRUE),
-                  selectInput("giftTypeInput", "Gift Type", 
-                              choices = gift_type, selected = gift_type,multiple = TRUE),
-                  selectInput("giftChannelInput", "Gift Channel", 
-                              choices = gift_channel, selected = gift_channel,multiple = TRUE),
-                  selectInput("paymentTypeInput", "Payment Type", 
-                              choices = payment_type, selected = payment_type,multiple = TRUE),
-                  submitButton("Submit")
+                             selectInput("campaignInput", "Campaign", 
+                                         choices = campaign, selected = campaign, multiple = TRUE),
+                             selectInput("primaryUnitInput", "Primary Unit", 
+                                         choices = primary_unit, selected = primary_unit,multiple = TRUE),
+                             selectInput("giftTypeInput", "Gift Type", 
+                                         choices = gift_type, selected = gift_type,multiple = TRUE),
+                             selectInput("giftChannelInput", "Gift Channel", 
+                                         choices = gift_channel, selected = gift_channel,multiple = TRUE),
+                             selectInput("paymentTypeInput", "Payment Type", 
+                                         choices = payment_type, selected = payment_type,multiple = TRUE),
+                             submitButton("Submit")
                 ),
                 mainPanel(
                   fluidRow(
@@ -92,22 +92,22 @@ ui <- dashboardPage(
                   )
                 )
               )
-            ),
-
+      ),
+      
       #=========
       # Overview
       #=========
       tabItem(tabName = "overview",
-                fluidRow(
-                  valueBoxOutput("interactionTypeBox"),
-                  valueBoxOutput("interactionSummaryBox"),
-                  valueBoxOutput("campaignBox")
-                ),
-                fluidRow(
-                  valueBoxOutput("appealBox"),
-                  valueBoxOutput("primaryUnitBox"),
-                  valueBoxOutput("giftChannelBox")
-                ),
+              fluidRow(
+                valueBoxOutput("interactionTypeBox"),
+                valueBoxOutput("interactionSummaryBox"),
+                valueBoxOutput("campaignBox")
+              ),
+              fluidRow(
+                valueBoxOutput("appealBox"),
+                valueBoxOutput("primaryUnitBox"),
+                valueBoxOutput("giftChannelBox")
+              ),
               fluidRow(
                 valueBoxOutput("paymentTypeBox"),
                 valueBoxOutput("giftDesgnationBox"),
@@ -120,151 +120,170 @@ ui <- dashboardPage(
                 plotlyOutput("giftOverviewOutput")
               )
               
-              ), 
+      ), 
       tabItem(tabName = "interaction",
               tabsetPanel(type = "tabs",
                           tabPanel(h4("Interaction Trends",style="text-align: center;"), 
-                                   plotlyOutput("interactionTrendPlot")),
+                                   plotlyOutput("interactionOverviewPlot"),
+                                   plotlyOutput("interactionYearPlot"),
+                                   plotlyOutput("interactionQuarterPlot"), 
+                                   plotlyOutput("interactionMonthPlot"), 
+                                   plotlyOutput("interactionDOWPlot")
+                          ),
                           tabPanel(h4("Interaction Insights",style="text-align: center;"), 
-                                   DT::dataTableOutput("interactionInsightsOutput")),
+                                            DT::dataTableOutput("interactionInsightsOutput")),
                           tabPanel(h4("Interaction Flow",style="text-align: center;"), 
-                                   DT::dataTableOutput("interactionFlowOutput"))
-                )
+                                            DT::dataTableOutput("interactionFlowOutput"))
+                          )
               ),
-      tabItem(tabName = "gift",
-              tabsetPanel(type = "tabs",
-                          tabPanel(h4("Gift Trends",style="text-align: center;"), 
-                                   plotlyOutput("giftTrendPlot")),
-                          tabPanel(h4("Gift Insights",style="text-align: center;"), 
-                                   DT::dataTableOutput("giftInsightsOutput")),
-                          tabPanel(h4("Gift Flow",style="text-align: center;"), 
-                                   DT::dataTableOutput("giftFlowOutput"))
-              )
-      )      
-
-            )
-         )
-       )
-################
-# Server
-################
-server <- function(input, output,session) {
-
-#=============
-# Overview
-#=============
-output$interactionTypeBox <- renderValueBox({
-  valueBox("Interaction Type", paste0(length(unique(interaction$INTERACTION_TYPE))), icon = icon("list"),color = "aqua")
-})  
-
-output$interactionSummaryBox <- renderValueBox({
-  valueBox("Interaction Summary", paste0(length(unique(interaction$INTERACTION_SUMMARY))), icon = icon("list"),color = "aqua")
-})
-
-output$giftAmtBox <- renderValueBox({
-  valueBox("Avg. Gift Amt.", paste0(round(mean(transaction$GIFT_AMOUNT)),2), icon = icon("thumbs-up"),color = "green")
-})  
+              tabItem(tabName = "gift",
+                      tabsetPanel(type = "tabs",
+                                  tabPanel(h4("Gift Trends",style="text-align: center;"), 
+                                           plotlyOutput("giftTrendPlot")),
+                                  tabPanel(h4("Gift Insights",style="text-align: center;"), 
+                                           DT::dataTableOutput("giftInsightsOutput")),
+                                  tabPanel(h4("Gift Flow",style="text-align: center;"), 
+                                           DT::dataTableOutput("giftFlowOutput"))
+                      )
+              )      
+              
+      )
+    )
+  )
+  ################
+  # Server
+  ################
+  server <- function(input, output,session) {
+    
+    #=============
+    # Overview
+    #=============
+    output$interactionTypeBox <- renderValueBox({
+      valueBox("Interaction Type", paste0(length(unique(interaction$INTERACTION_TYPE))), icon = icon("list"),color = "aqua")
+    })  
+    
+    output$interactionSummaryBox <- renderValueBox({
+      valueBox("Interaction Summary", paste0(length(unique(interaction$INTERACTION_SUMMARY))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$giftAmtBox <- renderValueBox({
+      valueBox("Avg. Gift Amt.", paste0(round(mean(transaction$GIFT_AMOUNT)),2), icon = icon("thumbs-up"),color = "green")
+    })  
+    
+    output$campaignBox <- renderValueBox({
+      valueBox("Campaign", paste0(length(unique(transaction$CAMPAIGN))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$appealBox <- renderValueBox({
+      valueBox("Appeals", paste0(length(unique(transaction$APPEAL))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$primaryUnitBox <- renderValueBox({
+      valueBox("Primary Unit", paste0(length(unique(transaction$PRIMARY_UNIT))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$giftChannelBox <- renderValueBox({
+      valueBox("Gift Channel", paste0(length(unique(transaction$GIFT_CHANNEL))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$paymentTypeBox <- renderValueBox({
+      valueBox("Payment Type", paste0(length(unique(transaction$PAYMENT_TYPE))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$giftDesgnationBox <- renderValueBox({
+      valueBox("Gift Designation", paste0(length(unique(transaction$GIFT_DESIGNATION))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$giftTypeBox <- renderValueBox({
+      valueBox("Gift Type", paste0(length(unique(transaction$GIFT_TYPE))), icon = icon("list"),color = "aqua")
+    })
+    
+    output$interactionOverviewOutput <- renderPlotly({
+      
+      g <- interaction %>%
+        group_by(INTERACTION_DATE) %>%
+        filter(INTERACTION_DATE <= today()) %>%
+        summarise(Total = sum(SUBSTANTIVE_INTERACTION)) %>%
+        select(INTERACTION_DATE, Total) %>%
+        ggplot(aes(x = INTERACTION_DATE ,y = Total))  +
+        geom_line() + theme_classic() + 
+        labs(x ="Interaction Type", y = "Total Interactions") 
+      theme(legend.text = element_text(size = 12),
+            legend.title = element_text(size = 12),
+            axis.title = element_text(size = 14),
+            axis.text = element_text(size = 12))
+      
+      ggplotly(g)
+      
+    })
+    
+    output$giftOverviewOutput <- renderPlotly({
+      
+      g <- transaction %>%
+        filter(GIFT_DATE <= today()) %>%
+        group_by(GIFT_DATE) %>%
+        summarise(Total = sum(GIFT_AMOUNT)) %>%
+        select(GIFT_DATE, Total) %>%
+        ggplot(aes(x = GIFT_DATE ,y = Total))  +
+        geom_line(stat ="identity") + theme_classic() + 
+        labs(x ="Gift Date", y = "Gift Amount") + scale_y_continuous(labels = scales::comma) + 
+        theme(legend.text = element_text(size = 12),
+              legend.title = element_text(size = 12),
+              axis.title = element_text(size = 14),
+              axis.text = element_text(size = 12))
+      
+      ggplotly(g)
+      
+    })
+    
+    #==========
+    # Interaction Trends
+    #==========
+    
+    interaction_df <- interaction %>%
+      mutate(year = lubridate::year(INTERACTION_DATE),
+             quarter = lubridate::quarter(INTERACTION_DATE),
+             month = lubridate::year(INTERACTION_DATE),
+             dow = lubridate::wday(INTERACTION_DATE, label=TRUE)
+      )
+    
+    
+    
+    
+    
+    
+    
+    #=============
+    # RFM analysis
+    #=============
+    output$rfmTable <- renderDataTable({
+      
+      rfm_data <- transaction %>%
+        filter(CAMPAIGN %in% input$campaignInput,
+               PRIMARY_UNIT %in% input$primaryUnitInput,
+               PAYMENT_TYPE %in%  input$paymentTypeInput,
+               GIFT_TYPE %in%  input$giftTypeInput,
+               GIFT_CHANNEL %in% input$giftChannelInput
+        ) %>%
+        na.omit()
+      
+      rfm_data_orders <- rfm_data %>%
+        mutate("customer_id" = CONTACT_ID, "order_date" = GIFT_DATE, "revenue" = GIFT_AMOUNT) %>%
+        select(customer_id, order_date, revenue) %>%
+        distinct()
+      
+      analysis_date <- lubridate::as_date(min(interaction$INTERACTION_DATE))
+      rfm_result <- rfm_table_order(rfm_data_orders, customer_id, order_date, revenue, analysis_date)
+      
+      df_rfm_segment <-  right_join(rfm_score,rfm_segment(rfm_result),by="rfm_score") %>%
+        select(customer_id, rfm_segment,rfm_score, transaction_count, recency_days, amount)
+      
+    })  
+    
+    #==================
+    # Forecast Results
+    #==================
+    
+  }             
   
-output$campaignBox <- renderValueBox({
-  valueBox("Campaign", paste0(length(unique(transaction$CAMPAIGN))), icon = icon("list"),color = "aqua")
-})
-
-output$appealBox <- renderValueBox({
-  valueBox("Appeals", paste0(length(unique(transaction$APPEAL))), icon = icon("list"),color = "aqua")
-})
-
-output$primaryUnitBox <- renderValueBox({
-  valueBox("Primary Unit", paste0(length(unique(transaction$PRIMARY_UNIT))), icon = icon("list"),color = "aqua")
-})
-
-output$giftChannelBox <- renderValueBox({
-  valueBox("Gift Channel", paste0(length(unique(transaction$GIFT_CHANNEL))), icon = icon("list"),color = "aqua")
-})
-
-output$paymentTypeBox <- renderValueBox({
-  valueBox("Payment Type", paste0(length(unique(transaction$PAYMENT_TYPE))), icon = icon("list"),color = "aqua")
-})
-
-output$giftDesgnationBox <- renderValueBox({
-  valueBox("Gift Designation", paste0(length(unique(transaction$GIFT_DESIGNATION))), icon = icon("list"),color = "aqua")
-})
-
-output$giftTypeBox <- renderValueBox({
-  valueBox("Gift Type", paste0(length(unique(transaction$GIFT_TYPE))), icon = icon("list"),color = "aqua")
-})
-
-output$interactionOverviewOutput <- renderPlotly({
-  
-  g <- interaction %>%
-    group_by(INTERACTION_DATE) %>%
-    filter(INTERACTION_DATE <= today()) %>%
-    summarise(Total = sum(SUBSTANTIVE_INTERACTION)) %>%
-    select(INTERACTION_DATE, Total) %>%
-    ggplot(aes(x = INTERACTION_DATE ,y = Total))  +
-    geom_line() + theme_classic() + 
-    labs(x ="Interaction Type", y = "Total Interactions") 
-  theme(legend.text = element_text(size = 12),
-        legend.title = element_text(size = 12),
-        axis.title = element_text(size = 14),
-        axis.text = element_text(size = 12))
-  
-  ggplotly(g)
-  
-})
-
-output$giftOverviewOutput <- renderPlotly({
-  
-  g <- transaction %>%
-    filter(GIFT_DATE <= today()) %>%
-    group_by(GIFT_DATE) %>%
-    summarise(Total = sum(GIFT_AMOUNT)) %>%
-    select(GIFT_DATE, Total) %>%
-    ggplot(aes(x = GIFT_DATE ,y = Total))  +
-    geom_line(stat ="identity") + theme_classic() + 
-    labs(x ="Gift Date", y = "Gift Amount") + scale_y_continuous(labels = scales::comma) + 
-    theme(legend.text = element_text(size = 12),
-          legend.title = element_text(size = 12),
-          axis.title = element_text(size = 14),
-          axis.text = element_text(size = 12))
-  
-  ggplotly(g)
-  
-})
-  
-  
-
-
-#=============
-# RFM analysis
-#=============
-output$rfmTable <- renderDataTable({
-  
-  rfm_data <- transaction %>%
-    filter(CAMPAIGN %in% input$campaignInput,
-           PRIMARY_UNIT %in% input$primaryUnitInput,
-           PAYMENT_TYPE %in%  input$paymentTypeInput,
-           GIFT_TYPE %in%  input$giftTypeInput,
-           GIFT_CHANNEL %in% input$giftChannelInput
-           ) %>%
-    na.omit()
-
-  rfm_data_orders <- rfm_data %>%
-    mutate("customer_id" = CONTACT_ID, "order_date" = GIFT_DATE, "revenue" = GIFT_AMOUNT) %>%
-    select(customer_id, order_date, revenue) %>%
-    distinct()
-  
-  analysis_date <- lubridate::as_date(min(interaction$INTERACTION_DATE))
-  rfm_result <- rfm_table_order(rfm_data_orders, customer_id, order_date, revenue, analysis_date)
-  
-  df_rfm_segment <-  right_join(rfm_score,rfm_segment(rfm_result),by="rfm_score") %>%
-    select(customer_id, rfm_segment,rfm_score, transaction_count, recency_days, amount)
-  
-})  
-
-#==================
-# Forecast Results
-#==================
-
-}             
-                
-shinyApp(ui, server)
+  shinyApp(ui, server)
