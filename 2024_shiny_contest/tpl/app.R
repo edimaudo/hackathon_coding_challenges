f57256e71212ac58735ca605baa6b0956cf8e96d
Edima <edimaudo@gmail.com>
2024-08-30 17:09:38 -0400

    Update app.R

--- 2024_shiny_contest/tpl/app.R
+++ 2024_shiny_contest/tpl/app.R
@@ -7,6 +7,7 @@ rm(list = ls())
 # Libraries
 ################
 packages <- c(
+  'rjson',
   'ggplot2',
   'corrplot',
   'tidyverse',
@@ -14,10 +15,8 @@ packages <- c(
   'shinydashboard',
   'DT',
   'readxl',
-  'forecast',
   'reshape2',
   'tidyr',
-  'Matrix',
   'lubridate',
   'plotly',
   'RColorBrewer',
@@ -34,7 +33,20 @@ for (package in packages) {
 ################
 # Load data
 ################
-
+tpl_clc <- read_csv("Computer_Learning_Centres.csv")
+tpl_dih <- read_csv("Digital_Innovation_Hubs.csv")
+tpl_kecl <- read_csv("KidsStop_Early_Literacy_Centres.csv")
+tpl_nib <- read_csv("Neighbourhood_Improvement_Area_Branches.csv")
+tpl <- read_csv("tpl-branch-general-information-2023.csv")
+tpl_branch_card_registration <- read_csv("tpl-card-registrations-annual-by-branch-2012-2022.csv")
+tpl_branch_circulation <- read_csv("tpl-circulation-annual-by-branch-2012-2022.csv")
+tpl_branch_eventfeed <- read_csv("tpl-events-feed.csv")
+tpl_branch_visit <- read_csv("tpl-visits-annual-by-branch-2012-2022.csv")
+tpl_branch_workstation <- read_csv("tpl-workstation-usage-annual-by-branch-2012-2022.csv")
+tpl_yag <- read_csv("Youth_Advisory_Groups_Locations.csv")
+tpl_yh <- read_csv("Youth_Hubs_Locations.csv")
+toronto_wellbeing <- read_csv("wellbeing-toronto-economics.csv")
+toronto_neighborhood <- fromJSON("Neighbourhoods.geojson")
 
 
 ################
