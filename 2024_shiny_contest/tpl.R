c49eab465db39e7fc7aaaa3ead3055d1d9f241fd
Edima <edimaudo@gmail.com>
2024-09-01 11:08:50 -0400

    Update tpl.R

--- 2024_shiny_contest/tpl.R
+++ 2024_shiny_contest/tpl.R
@@ -1,18 +1,45 @@
 rm(list = ls())
-#=============
-# Libraries 
-#=============
+################
+# Libraries
+################
 packages <- c(
-  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT','readxl',
-  'forecast','reshape2','tidyr','Matrix','lubridate','plotly','RColorBrewer',
-  'data.table','scales','dplyr'
+  'rjson',
+  'ggplot2',
+  'corrplot',
+  'tidyverse',
+  'shiny',
+  'shinydashboard',
+  'DT',
+  'readxl',
+  'reshape2',
+  'tidyr',
+  'lubridate',
+  'plotly',
+  'RColorBrewer',
+  'data.table',
+  'scales',
+  'dplyr'
 )
 for (package in packages) {
-  if (!require(package, character.only=T, quietly=T)) {
+  if (!require(package, character.only = T, quietly = T)) {
     install.packages(package)
-    library(package, character.only=T)
+    library(package, character.only = T)
   }
 }
-#=============
+################
 # Load data
-#=============
+################
+tpl_clc <- read_csv("Computer_Learning_Centres.csv")
+tpl_dih <- read_csv("Digital_Innovation_Hubs.csv")
+tpl_kecl <- read_csv("KidsStop_Early_Literacy_Centres.csv")
+tpl_nib <- read_csv("Neighbourhood_Improvement_Area_Branches.csv")
+tpl <- read_csv("tpl-branch-general-information-2023.csv")
+tpl_branch_card_registration <- read_csv("tpl-card-registrations-annual-by-branch-2012-2022.csv")
+tpl_branch_circulation <- read_csv("tpl-circulation-annual-by-branch-2012-2022.csv")
+tpl_branch_eventfeed <- read_csv("tpl-events-feed.csv")
+tpl_branch_visit <- read_csv("tpl-visits-annual-by-branch-2012-2022.csv")
+tpl_branch_workstation <- read_csv("tpl-workstation-usage-annual-by-branch-2012-2022.csv")
+tpl_yag <- read_csv("Youth_Advisory_Groups_Locations.csv")
+tpl_yh <- read_csv("Youth_Hubs_Locations.csv")
+toronto_wellbeing <- read_csv("wellbeing-toronto-economics.csv")
+toronto_neighborhood <- fromJSON("Neighbourhoods.geojson")
