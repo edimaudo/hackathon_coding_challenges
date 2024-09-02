0d9d7440cb1a026f503f8313a0feef8314a81142
Edima <edimaudo@gmail.com>
2024-09-02 18:20:25 -0400

    Update app.R

--- 2024_shiny_contest/tpl/app.R
+++ 2024_shiny_contest/tpl/app.R
@@ -36,7 +36,6 @@ tpl_branch_workstation <- read_csv("tpl-workstation-usage-annual-by-branch-2012-
 tpl_yag <- read_csv("Youth_Advisory_Groups_Locations.csv")
 tpl_yh <- read_csv("Youth_Hubs_Locations.csv")
 toronto_wellbeing <- read_csv("wellbeing-toronto-economics.csv")
-#toronto_neighborhood <- fromJSON("Neighbourhoods.geojson")
 
 
 ################
@@ -51,11 +50,30 @@ ui <- dashboardPage(
                           class = "dropdown")),
   dashboardSidebar(
     sidebarMenu(
+      menuItem("Overview", tabName = "overview", icon = icon("list")),
       menuItem("About", tabName = "about", icon = icon("th"))
     )
   ),
   dashboardBody(
+    #========  
+    # Overview
+    #========
     tabItems(
+      tabItem(tabName = "overview",
+              fluidRow(
+                valueBoxOutput("libraryBox"),
+                valueBoxOutput("clcBox"),
+                valueBoxOutput("keclBox"),
+                valueBoxOutput("nibBox"),
+                valueBoxOutput("dihBox"),
+                valueBoxOutput("yagBox"),
+                valueBoxOutput("yhBox")
+              ),
+              fluidRow(
+                #plotlyOutput("submissionOutput"),  
+              ),
+              
+      ),
       tabItem(tabName = "about",includeMarkdown("about.md"),hr())
     )
   )
@@ -66,6 +84,64 @@ ui <- dashboardPage(
 # Server
 ################
 server <- function(input, output, session) {
+  #-----------
+  # Overview
+  #-----------
+  # of libraries
+  output$libraryBox <- renderValueBox({
+    tpl_library <- tpl %>%
+      filter(PhysicalBranch == 1) %>%
+      select(BranchName)
+    
+    valueBox(
+      "Libraries", paste0(length(unique(tpl_library$BranchName))), icon = icon("book"),
+      color = "aqua"
+    )
+  })
+  # of  Computer_Learning_Centres
+  output$clcBox <- renderValueBox({
+    valueBox(
+      "Computer Learning Centres", paste0(length(unique(tpl_clc$`Branch Name`))), icon = icon("computer"),
+      color = "aqua"
+    )
+  })  
+  # of KidsStop_Early_Literacy_Centres
+  output$keclBox <- renderValueBox({
+    valueBox(
+      "Early Literacy Centres", paste0(length(unique(tpl_kecl$`Branch Name`))), icon = icon("child"),
+      color = "aqua"
+    )
+  })  
+  
+  # of Neighbourhood_Improvement_Area_Branches
+  output$nibBox <- renderValueBox({
+    valueBox(
+      "Improvement Branches", paste0(length(unique(tpl_nib$`Branch Name`))), icon = icon("thumbs-up"),
+      color = "aqua"
+    )
+  })  
+  # of Digital_Innovation_Hubs
+  output$dihBox <- renderValueBox({
+    valueBox(
+      "Digital Innovation Hub", paste0(length(unique(tpl_dih$`Branch Name`))), icon = icon("lightbulb"),
+      color = "aqua"
+    )
+  })    
+  # of Youth_Advisory_Groups_Locations
+  output$yagBox <- renderValueBox({
+    valueBox(
+      "Youth Advisory", paste0(length(unique(tpl_yag$`Branch Name`))), icon = icon("person"),
+      color = "aqua"
+    )
+  })   
+  # of Youth_Hubs_Locations
+  output$yhBox <- renderValueBox({
+    valueBox(
+      "Youth Hub", paste0(length(unique(tpl_yh$`Branch Name`))), icon = icon("person"),
+      color = "aqua"
+    )
+  }) 
+
 }
 
 shinyApp(ui, server)
