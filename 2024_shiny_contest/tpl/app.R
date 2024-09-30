6d948ad00c267e3aeb638864612607e19d8c5e8e
Edima <edimaudo@gmail.com>
2024-09-30 16:27:18 -0400

    Update app.R

--- 2024_shiny_contest/tpl/app.R
+++ 2024_shiny_contest/tpl/app.R
@@ -1,26 +1,44 @@
-
-
 ################
 # Shiny web app which provides 
 # insights about  Toronto Public Library
 ################
-rm(list = ls())
+#rm(list = ls())
 ################
 # Libraries
 ################
-packages <- c(
+library(ggplot2)
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(lubridate)
+library(plotly)
+library(stopwords)
+library(tidytext)
+library(stringr)
+library(wordcloud)
+library(wordcloud2)
+library(textmineR)
+library(topicmodels)
+library(textclean)
+library(tm)
 
-  'ggplot2','tidyverse','shiny','shinydashboard','DT',
-  'lubridate','plotly','RColorBrewer','scales','stopwords',
-  'tidytext','stringr','wordcloud','wordcloud2',
-  'SnowballC','textmineR','topicmodels','textclean','tm'
-)
-for (package in packages) {
-  if (!require(package, character.only = T, quietly = T)) {
-    install.packages(package)
-    library(package, character.only = T)
-  }
-}
+
+# packages <- c(
+# 
+#   'ggplot2','tidyverse',
+#   'shiny',
+#   'shinydashboard','DT',
+#   'lubridate','plotly','RColorBrewer','scales','stopwords',
+#   'tidytext','stringr','wordcloud','wordcloud2',
+#   'SnowballC','textmineR','topicmodels','textclean','tm'
+# )
+# for (package in packages) {
+#   if (!require(package, character.only = T, quietly = T)) {
+#     install.packages(package)
+#     library(package, character.only = T)
+#   }
+# }
 ################
 # Data
 ################
