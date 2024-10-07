4d37fce5ff473e455bb86b70e8382f4dffb5e01f
Edima <edimaudo@gmail.com>
2024-10-07 18:21:04 -0400

    Create app.R

---
+++ OTF/app/app.R
@@ -0,0 +1,26 @@
+################
+# Ontario Trillium Foundation Insights
+################
+
+################
+# Libraries
+################
+library(ggplot2)
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(lubridate)
+library(plotly)
+library(stopwords)
+library(tidytext)
+library(stringr)
+library(wordcloud)
+library(wordcloud2)
+library(textmineR)
+library(topicmodels)
+library(textclean)
+library(tm)
+library(htmltools)
+library(markdown)
+library(scales)
