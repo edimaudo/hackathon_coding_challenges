61d85c3e510cb71163111c1c9a35a4dafef8db79
Edima <edimaudo@gmail.com>
2024-11-09 14:50:12 -0500

    Update app.R

--- MTA/mta/app.R
+++ MTA/mta/app.R
@@ -1,51 +1,26 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+################
+# Shiny web app which provides MTA Insights
+################
+#rm(list = ls())
+################
+# Libraries
+################
+# library(ggplot2)
+# library(tidyverse)
+# library(shiny)
+# library(shinydashboard)
+# library(DT)
+# library(lubridate)
+# library(plotly)
+# library(stopwords)
+# library(tidytext)
+# library(stringr)
+# library(wordcloud)
+# library(wordcloud2)
+# library(textmineR)
+# library(topicmodels)
+# library(textclean)
+# library(tm)
+# library(htmltools)
+# library(markdown)
+# library(scales)
