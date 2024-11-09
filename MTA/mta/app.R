7dcef9057dc95eed066b63e198f144d4ac3862f5
Edima <edimaudo@gmail.com>
2024-11-09 18:50:19 -0500

    Update app.R

--- MTA/mta/app.R
+++ MTA/mta/app.R
@@ -24,3 +24,21 @@
 # library(htmltools)
 # library(markdown)
 # library(scales)
+
+# remove when publihsing
+packages <- c(
+
+  'ggplot2','tidyverse',
+  'shiny',
+  'shinydashboard','DT',
+  'lubridate','plotly','RColorBrewer','scales','stopwords',
+  'tidytext','stringr','wordcloud','wordcloud2',
+  'SnowballC','textmineR','topicmodels','textclean','tm'
+)
+for (package in packages) {
+  if (!require(package, character.only = T, quietly = T)) {
+    install.packages(package)
+    library(package, character.only = T)
+  }
+}
+
