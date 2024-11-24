b2b7838e3d06f1c4f7e841b801c18d3ebf3309b4
Edima <edimaudo@gmail.com>
2024-11-24 12:53:43 -0500

    Update 3_Wine_Predictor.py
    Added wine quality model

--- wine_quality/Pages/3_Wine_Predictor.py
+++ wine_quality/Pages/3_Wine_Predictor.py
@@ -2,10 +2,23 @@
 from utils import * 
 st.title(APP_NAME)
 
+columns_data = ['fixed acidity',	
+'volatile acidity',	
+'citric acid',	
+'residual sugar',	
+'chlorides',	
+'free sulfur dioxide',	
+'total sulfur dioxide',	
+'density',	
+'pH',	
+'sulphates',	
+'alcohol']
+
 col1, col2 = st.columns([3, 1]) 
 with col2:
-	clicked = st.button("Run Wine Quality Prediction")
+	clicked = st.button("Run Prediction")
 	with st.expander(APP_FILTERS):
+		wine_type = st.radio("Wine Type",['White Wine','Red Wine'])
 		fixed_acidity = st.slider("Fixed Acidity", 1, 15, 0.5)
 		volatile_acidity = st.slider("Volatile Acidity", 0.05, 1.5, 0.01)	
 		citric_acid	= st.slider("Citric Acid", 0.5, 2, 0.05)
@@ -17,4 +30,31 @@ with col2:
 		pH	= st.slider("pH", 2.5, 5, 0.05)
 		sulphates = st.slider("Sulphates", 0.2, 1.5, 0.05)
 		alcohol = st.slider("Alcohol", 7, 14, 0.05)
+
+with col1:
+    st.header(PREDICTION_NAME_HEADER)
+    # Model information
+    model_data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
+    # scale data
+    df_scale = model_data
+    scaler = preprocessing.MinMaxScaler()
+	df_scale = scaler.fit_transform(df_scale)
+	df_scale = pd.DataFrame(df_scale)
+	df_scale.reset_index(drop=True, inplace=True)
+
+    if clicked:
+    	# Load model
+    	if wine_type == "White Wine":
+        	saved_model = load_model('white_wine')
+        else:
+        	saved_model = load_model('red_wine')
+
+        info_df = pd.DataFrame(columns = columns_data,index = ['a'])
+    	info_df.loc['a'] = df_scale
+        # Prediction
+        new_prediction = predict_model(saved_model, data=info_df)
+        wine_quality_output = new_prediction['Label'][0]
+        st.write("Based on your selection")
+        st.metric("The Predicted Wine Quality is: ",wine_quality_output)
+
 
