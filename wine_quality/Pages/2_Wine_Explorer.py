cb61d32e53c5d1745de12573c7ba5c549fa2df72
Edima <edimaudo@gmail.com>
2024-11-25 18:43:41 -0500

    Update 2_Wine_Explorer.py
    added correlation plot

--- wine_quality/Pages/2_Wine_Explorer.py
+++ wine_quality/Pages/2_Wine_Explorer.py
@@ -1,7 +1,7 @@
 from utils import * 
 st.title(APP_NAME)
 
-st.subheader(WINE_EXPLORATION_HEADER)
+st.header(WINE_EXPLORATION_HEADER)
 
 
 with st.container():
@@ -11,4 +11,17 @@ with st.container():
     else:
     	df = red_df
 
-    
+
+    st.subheader("**Correlation**")
+    df_corr = df.corr().round(1)
+    # Mask to matrix
+	mask = np.zeros_like(df_corr, dtype=bool)
+	mask[np.triu_indices_from(mask)] = True
+	# Viz
+	df_corr_viz = df_corr.mask(mask).dropna(how='all').dropna('columns', how='all')
+	fig = px.imshow(df_corr_viz, text_auto=True)
+	#fig = px.imshow(df_corr,text_auto=True)
+    st.plotly_chart(fig)
+
+
+    #st.subheader("**Wine Clusters**")
