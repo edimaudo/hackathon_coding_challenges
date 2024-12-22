3a5910ab6c49391697011952b92124c42d7b05b7
Edima <edimaudo@gmail.com>
2024-12-22 16:01:38 -0500

    Create hockey_analysis.R

---
+++ stathlete_big_data_cup_2025/hockey_analysis.R
@@ -0,0 +1,13 @@
+################
+# Packages
+################
+packages <- c('ggplot2','corrplot','tidyverse','readxl','DT',
+              'RColorBrewer','shiny','shinydashboard','scales','dplyr',
+              'forecast','lubridate','stopwords','tidytext','stringr',
+              'reshape2', 'textmineR','topicmodels','textclean')
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
