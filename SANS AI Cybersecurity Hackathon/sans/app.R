c21a3fe50fbfeb56495b3a83f207aa3d8356e200
Edima <edimaudo@gmail.com>
2025-03-03 18:32:16 -0500

    Update app.R

--- SANS AI Cybersecurity Hackathon/sans/app.R
+++ SANS AI Cybersecurity Hackathon/sans/app.R
@@ -25,3 +25,27 @@ for (package in packages) {
     library(package, character.only = T)
   }
 }
+
+
+#### Data ####
+
+####### UI #########
+ui <- dashboardPage(
+  dashboardHeader(title = "Greenhouse Gas Emissions Data",
+                  tags$li(a(href = 'https://climatedata.imf.org/datasets/c8579761f19740dfbe4418b205654ddf_0/about',
+                            img(src = 'https://imf-dataviz.maps.arcgis.com/sharing/rest/content/items/bf9aa914b237454babc8ed059575c1a7/resources/imf-climate-logo.png',
+                                title = "Home", height = "30px"),
+                            style = "padding-top:10px; padding-bottom:10px;"),
+                          class = "dropdown")
+  ),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About", tabName = "about", icon = icon("th")),
+      menuItem("Overview", tabName = "overview", icon = icon("th")),
+      menuItem("Details", tabName = "detail", icon = icon("list")), 
+      menuItem("Gas Type Analysis", tabName = "analysis", icon = icon("list")),
+      menuSubItem("Gas Type Forecasting", tabName = "riderhsip_forecast",icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
