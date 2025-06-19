59daced4e78f1e52a40e9dee2a1ec0a41df224e6
Edima <edimaudo@gmail.com>
2025-06-19 12:31:00 -0400

    Update app.R

--- Apra/2025/apra_2025/app.R
+++ Apra/2025/apra_2025/app.R
@@ -31,9 +31,10 @@ rm(list = ls())
 # library(xts)
 # library(dplyr)
 # library(treemapify)
+# library(bslib)
 
 packages <- c(
-  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT',
+  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','bslib','DT',
   'mlbench','caTools','gridExtra','doParallel','grid','reshape2',
   'caret','tidyr','Matrix','lubridate','plotly','RColorBrewer',
   'data.table','scales','rfm','forecast','TTR','xts','dplyr', 'treemapify'
@@ -52,6 +53,12 @@ gift <- read_csv("gift_transactions_table.csv")
 video <- read_csv("video_email_data_table.csv")
 constituent <- read_csv("constituent_profiles_table.csv")
 
+# Data Information
+# RFM
+segment_titles <- c("First Grade", "Loyal", "Likely to be Loyal",
+                    "New Ones", "Could be Promising", "Require Assistance", "Getting Less Frequent",
+                    "Almost Out", "Can't Lose Them", "Don’t Show Up at All")
+
 ################
 # UI
 ################
@@ -69,7 +76,7 @@ ui <- dashboardPage(
       menuSubItem("Weekly", tabName = "weekly"),
       menuSubItem("Monthly", tabName = "monthly"),
       menuSubItem("Yearly", tabName = "yearly"),
-      menuItem("Donor Portfolio", tabName = "segmentation", icon = icon("list")),
+      menuItem("Donor Portfolio", tabName = "segment", icon = icon("list")),
       menuItem("Donor Prediction", tabName = "forecast_overview", icon = icon("list")),
       menuSubItem("Donation Forecasting ", tabName = "donation_forecast"),
       menuSubItem("Next Best Donation", tabName = "donation_prediction")
@@ -92,19 +99,20 @@ ui <- dashboardPage(
       tabItem(tabName = "segment",
               sidebarLayout(
                 sidebarPanel(width = 3,
-                             selectInput("campaignInput", "Campaign", 
-                                         choices = campaign, selected = campaign, multiple = TRUE),
-                             selectInput("primaryUnitInput", "Primary Unit", 
-                                         choices = primary_unit, selected = primary_unit,multiple = TRUE),
-                             selectInput("giftTypeInput", "Gift Type", 
-                                         choices = gift_type, selected = gift_type,multiple = TRUE),
-                             selectInput("giftChannelInput", "Gift Channel", 
-                                         choices = gift_channel, selected = gift_channel,multiple = TRUE),
-                             selectInput("paymentTypeInput", "Payment Type", 
-                                         choices = payment_type, selected = payment_type,multiple = TRUE),
+                             selectInput("rfmInput", "Portfolios", 
+                                         choices = segment_titles, selected = segment_titles, multiple = TRUE),
                              submitButton("Submit")
                 ),
                 mainPanel(
+                  fluidRow(
+                    valueBox(title="Avg. Recency",value=uiOutput("valueRecency")), #theme="bg-gradient-blue"
+                    valueBox(title="Avg. Frequency",value=uiOutput("valueFrequency")),
+                    valueBox(title="Avg. Monetary",value=uiOutput("valueMonetary")),
+                  ),
+                  fluidRow(
+                    plotlyOutput("rfmTreemap")),
+                    plotlyOutput("rfmBarChart")),
+                  ),
                   fluidRow(
                     DT::dataTableOutput("rfmTable")
                   )
@@ -115,8 +123,7 @@ ui <- dashboardPage(
       # Donor Prediction
       #======== 
     )
-  )
-)
+  
 
  
       
@@ -124,6 +131,34 @@ ui <- dashboardPage(
 ################
 # Server
 ################
-server <- function(input, output,session) {}
+server <- function(input, output,session) {
+
+  #======== 
+  # Donor Portfolio
+  #========
+  
+  forecast_analysis_df1  <- reactive({
+    
+  })
+    forecast_data <- df2 %>%
+      filter(Country %in% c(input$countryInput) , 
+             Industry %in% c(input$industryInput),
+             `Gas Type` %in% c(input$gasInput)) %>%
+      group_by(Year) %>%
+      summarize(Total = sum(Total)) %>%
+      select(Year, Total)
+    
+    df_xts <- xts(x = forecast_data$Total, 
+                  order.by = (forecast_data$Year))
+    
+    
+  
+  output$rfmTable <- renderDataTable({
+    
+ 
+    
+  }) 
+
+}
 
 shinyApp(ui, server)
