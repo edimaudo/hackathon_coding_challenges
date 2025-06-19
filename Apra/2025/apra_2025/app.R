8346b3c98be16a8b872a47ade113bafce6efc5d8
Edima <edimaudo@gmail.com>
2025-06-19 11:00:58 -0400

    Update app.R

--- Apra/2025/apra_2025/app.R
+++ Apra/2025/apra_2025/app.R
@@ -3,12 +3,38 @@
 # for Apra data science challenge
 #=========================================
 rm(list = ls())
-#=============
+################
 # Packages 
-#=============
+################
+# library(ggplot2)
+# library(corrplot)
+# library(tidyverse)
+# library(shiny)
+# library(shinydashboard)
+# library(mlbench)
+# library(caTools)
+# library(gridExtra)
+# library(doParallel)
+# library(grid)
+# library(reshape2)
+# library(caret)
+# library(tidyr)
+# library(Matrix)
+# library(lubridate)
+# library(plotly)
+# library(RColorBrewer)
+# library(data.table)
+# library(scales)
+# library(rfm)
+# library(forecast)
+# library(TTR)
+# library(xts)
+# library(dplyr)
+# library(treemapify)
+
 packages <- c(
-  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT','readxl',
-  'mlbench','caTools','gridExtra','doParallel','grid','forecast','reshape2',
+  'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT',
+  'mlbench','caTools','gridExtra','doParallel','grid','reshape2',
   'caret','tidyr','Matrix','lubridate','plotly','RColorBrewer',
   'data.table','scales','rfm','forecast','TTR','xts','dplyr', 'treemapify'
 )
@@ -18,9 +44,9 @@ for (package in packages) {
     library(package, character.only=T)
   }
 }
-#=============
+################
 # Load Data
-#=============
+################
 crm <- read_csv("CRM_interacions_table.csv")
 gift <- read_csv("gift_transactions_table.csv")
 video <- read_csv("video_email_data_table.csv")
@@ -30,7 +56,7 @@ constituent <- read_csv("constituent_profiles_table.csv")
 # UI
 ################
 ui <- dashboardPage(
-  dashboardHeader(title = "Apra Data Science Challenge",
+  dashboardHeader(title = "Apra Challenge 2025",
                   tags$li(a(href = 'https://www.aprahome.org',
                             img(src = 'https://www.aprahome.org/Portals/_default/skins/siteskin/images/logo.png',
                                 title = "Home", height = "30px"),
@@ -43,7 +69,7 @@ ui <- dashboardPage(
       menuSubItem("Weekly", tabName = "weekly"),
       menuSubItem("Monthly", tabName = "monthly"),
       menuSubItem("Yearly", tabName = "yearly"),
-      menuItem("Customer Segmentation", tabName = "segment", icon = icon("list")),
+      menuItem("Donor Portfolio", tabName = "segmentation", icon = icon("list")),
       menuItem("Donor Prediction", tabName = "forecast_overview", icon = icon("list")),
       menuSubItem("Donation Forecasting ", tabName = "donation_forecast"),
       menuSubItem("Next Best Donation", tabName = "donation_prediction")
@@ -55,6 +81,39 @@ ui <- dashboardPage(
       # About
       #======== 
       tabItem(tabName = "about",includeMarkdown("about.md"),hr()), 
+      
+      #======== 
+      # Overview
+      #======== 
+      
+      #======== 
+      # Donor Portfolio
+      #======== 
+      tabItem(tabName = "segment",
+              sidebarLayout(
+                sidebarPanel(width = 3,
+                             selectInput("campaignInput", "Campaign", 
+                                         choices = campaign, selected = campaign, multiple = TRUE),
+                             selectInput("primaryUnitInput", "Primary Unit", 
+                                         choices = primary_unit, selected = primary_unit,multiple = TRUE),
+                             selectInput("giftTypeInput", "Gift Type", 
+                                         choices = gift_type, selected = gift_type,multiple = TRUE),
+                             selectInput("giftChannelInput", "Gift Channel", 
+                                         choices = gift_channel, selected = gift_channel,multiple = TRUE),
+                             selectInput("paymentTypeInput", "Payment Type", 
+                                         choices = payment_type, selected = payment_type,multiple = TRUE),
+                             submitButton("Submit")
+                ),
+                mainPanel(
+                  fluidRow(
+                    DT::dataTableOutput("rfmTable")
+                  )
+                )
+              )
+      ),
+      #======== 
+      # Donor Prediction
+      #======== 
     )
   )
 )
