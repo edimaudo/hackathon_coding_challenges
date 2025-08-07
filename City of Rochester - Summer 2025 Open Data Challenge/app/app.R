6d0eccfb91877c3f3089593b0529bdfced21cac5
Edima <edimaudo@gmail.com>
2025-08-07 12:05:12 -0400

    Update app.R

--- City of Rochester - Summer 2025 Open Data Challenge/app/app.R
+++ City of Rochester - Summer 2025 Open Data Challenge/app/app.R
@@ -47,3 +47,61 @@ for (package in packages) {
 ################ Load Data ################
 parks <- read_csv("parks_playground.csv.csv")
 tree <- read_csv("trees.csv.csv")
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Summer 2025 Open Data Challenge",
+                  tags$li(a(href = 'https://data.cityofrochester.gov',
+                            img(src = 'https://www.arcgis.com/sharing/rest/content/items/273dc226904e43f0a83baecf54a31397/resources/logo.png?v=1754582471296',
+                                title = "Home", height = "30px"),
+                            style = "padding-top:10px; padding-bottom:10px;"),
+                          class = "dropdown")),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About", tabName = "about", icon = icon("house")),
+      menuItem("Park Overiew", tabName = "park_overview", icon = icon("th")),
+      menuItem("Park Insights", tabName = "park_insight", icon = icon("thumbs-up")),
+
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      ######### About #########
+      tabItem(tabName = "about",includeMarkdown("about.md"),hr()), 
+      
+      ######### Park Overview ######### 
+      tabItem(tabName = "park_overview",
+              sidebarLayout(
+                sidebarPanel(width = 2,
+                             sliderInput("yearDonationInput","Year", min = 2015, max = 2025, 
+                                         value = c(2015,2025), step = 1),
+                             selectInput("monthDonationInput", "Month", 
+                                         choices = month_titles, selected = month_titles, multiple = TRUE),
+                             submitButton("Submit")
+                ),
+                mainPanel(width = 10,
+                          layout_column_wrap(width = 1/2,
+                                             plotlyOutput("giftCRMPlot"),
+                                             plotlyOutput("giftYearPlot"),
+                          ),
+                          br(),br(),
+                          layout_columns(
+                            plotlyOutput("giftMonthPlot"),
+                            plotlyOutput("giftDOWPlot"),
+                          )
+                )
+              )
+              
+      )
+    )
+  ) 
+ )
+
+    
+    
+################  Server ################
+server <- function(input, output,session) {
+      
+}
+    
+shinyApp(ui, server)
